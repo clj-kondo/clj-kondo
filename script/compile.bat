9221bea7c3619140b94fdae038e6cb8e1f0c67b8
Michiel Borkent <michielborkent@gmail.com>
2019-05-15 13:41:22 +0200

    script\compile.bat

--- script/compile.bat
+++ script/compile.bat
@@ -1,6 +1,6 @@
 @echo off
 if "%GRAALVM_HOME%"=="" ( 
-    echo "Please set GRAALVM_HOME"
+    echo Please set GRAALVM_HOME
     exit /b
 )
 set %JAVA_HOME%=%GRAAL_VM_HOME%\bin
@@ -14,9 +14,16 @@ call lein uberjar
 
 call %GRAALVM_HOME%\bin\native-image.cmd ^
   -jar target/clj-kondo-%CLJ_KONDO_VERSION%-standalone.jar ^
-  -H:Name=clj-kondo -H:ReflectionConfigurationFiles=reflection.json ^
-  --initialize-at-build-time "-H:IncludeResources=clj_kondo/impl/cache/built_in/.*" ^
-  -H:Log=registerResource:
+  -H:Name=clj-kondo ^
+  -H:+ReportExceptionStackTraces ^
+  -J-Dclojure.spec.skip-macros=true ^
+  -J-Dclojure.compiler.direct-linking=true ^
+  "-H:IncludeResources=clj_kondo/impl/cache/built_in/.*" ^
+  -H:ReflectionConfigurationFiles=reflection.json ^
+  --initialize-at-build-time  ^
+  -H:Log=registerResource: ^
+  --verbose ^
+  "-J-Xmx3g"
 
 echo Creating zip archive
 
