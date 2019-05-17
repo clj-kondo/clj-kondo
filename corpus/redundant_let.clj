16e131573f9144889a5f4fa9dccc698ef970d089
Michiel Borkent <michielborkent@gmail.com>
2019-05-17 10:32:38 +0200

    only report redundant let when single let child

--- corpus/redundant_let.clj
+++ corpus/redundant_let.clj
@@ -10,3 +10,11 @@
 (let [x 1]
   ;; (println "hello")
   (let [y 2]))
+
+;; this one should not be reported:
+(defn two-cases []
+  (let [resource 1]
+    (let [result [1 (inc resource) 3]]
+      (println (= 2 (count result))))
+    (let [result [1 (inc resource)]]
+      (println (= 3 (count result))))))
