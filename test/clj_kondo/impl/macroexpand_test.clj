c407165dff4e990196f13fb1d74be1c1ff9d9e07
Michiel Borkent <michielborkent@gmail.com>
2019-05-21 11:08:06 +0200

    remove duplicate require

--- test/clj_kondo/impl/macroexpand_test.clj
+++ test/clj_kondo/impl/macroexpand_test.clj
@@ -3,8 +3,7 @@
    [clj-kondo.impl.macroexpand :as macroexpand]
    [clj-kondo.impl.utils :refer [parse-string]]
    [clojure.test :as t :refer [deftest is testing]]
-   [rewrite-clj.node.protocols :refer [tag]]
-   [rewrite-clj.node.protocols :as node]))
+   [rewrite-clj.node.protocols :as node :refer [tag]]))
 
 (defn location [node]
   (let [m (meta node)]
