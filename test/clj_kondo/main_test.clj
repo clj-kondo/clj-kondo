3b0ef34c07c3761102092ac602ee612bcfb08919
Michiel Borkent <michielborkent@gmail.com>
2019-07-24 20:37:55 +0200

    [#366] turn off unresolved symbol warnings for type hints in CLJS bindings

--- test/clj_kondo/main_test.clj
+++ test/clj_kondo/main_test.clj
@@ -1880,7 +1880,10 @@
   (is (empty? (lint! "(ns foo) (defn foo [] (ns bar (:require [clojure.string :as s])))")))
   (is (empty? (lint! "(defn foo [x y z] ^{:a x :b y :c z} [1 2 3])")))
   (is (empty? (lint! (io/file "corpus" "deftest.cljc")
-                     '{:linters {:unresolved-symbol {:level :error}}}))))
+                     '{:linters {:unresolved-symbol {:level :error}}})))
+  (is (empty? (lint! "(fn [^js x] x)"
+                     '{:linters {:unresolved-symbol {:level :error}}}
+                     "--lang" "cljs"))))
 
 (deftest file-error-test
   (assert-submaps '({:file "not-existing.clj",
