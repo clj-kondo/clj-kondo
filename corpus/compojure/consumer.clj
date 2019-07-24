731954a6b1d2270cf5f91b4b0409c15f3bcfe8f4
Michiel Borkent <michielborkent@gmail.com>
2019-07-24 14:52:58 +0200

    pic

--- corpus/compojure/consumer.clj
+++ corpus/compojure/consumer.clj
@@ -14,7 +14,7 @@
   (GET "/" [] "<h1>Hello World</h1>")
   (GET "/user" {{:keys [user-id]} :session} ;; <- no unresolved symbols due to config
        (str "The current user is " user-id) ;; <- no unresolved symbols due to config
-       x))  ;; <- this symbol is still unresolved
+       x)) ;; <- this symbol is still unresolved
 
 (def app
   (handler/site app-routes))  ;; <- defroutes is linted as def, so clj-kondo
