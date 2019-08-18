3fd613ff329669c70a9ecdea86e4686f2b01b5e2
Michiel Borkent <michielborkent@gmail.com>
2019-08-18 22:49:50 +0200

    [#422] fix recur arity false positive with core.async/alt

--- src/clj_kondo/impl/analyzer/core_async.clj
+++ src/clj_kondo/impl/analyzer/core_async.clj
@@ -2,24 +2,29 @@
   (:require
    [clj-kondo.impl.utils :refer [tag]]))
 
-(defn analyze-alt-val [ctx analyze-expression** analyze-fn-body expr]
-  (let [[fst-child _] (:children expr)]
+(defn analyze-alt-val [ctx analyze-expression** extract-bindings expr]
+  (let [[fst-child & rest-children] (:children expr)]
     (if (and
          fst-child
          (= :list (tag expr))
          (= :vector (tag fst-child)))
-      (:parsed (analyze-fn-body ctx expr))
+      ;; analyze syntax like: ([v ch] 'do_something_with_v_or_ch)
+      ;; NOTE: this syntax also supports destructuring:
+      ;; (async/alt!! (doto (async/chan) (async/put! {:a 1}))  ([{:keys [:a]} ch] [a ch]))
+      (let [bindings (extract-bindings ctx fst-child)
+            ctx (update ctx :bindings into bindings)]
+        (mapcat #(analyze-expression** ctx %) rest-children))
       (analyze-expression** ctx expr))))
 
 (defn analyze-alt! [ctx expr]
   (let [{:keys [:analyze-expression**
-                :analyze-fn-body]} ctx
+                :extract-bindings]} ctx
         children (next (:children expr))
         pairs (partition-all 2 children)]
     (for [[k v] pairs
           analyzed-value (concat (analyze-expression** ctx k)
                                  (analyze-alt-val ctx analyze-expression**
-                                                  analyze-fn-body v))]
+                                                  extract-bindings v))]
       analyzed-value)))
 
 (comment
