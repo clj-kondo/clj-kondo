c5dba5467750732cec458f9ff1b783b18b20e507
Michiel Borkent <michielborkent@gmail.com>
2019-09-12 10:39:03 +0200

    fix test

--- test/clj_kondo/main_test.clj
+++ test/clj_kondo/main_test.clj
@@ -594,7 +594,7 @@
    (lint! "(let [fn (fn [])] (fn 1))"))
   (is (empty? (lint! "(let [f #(apply println % %&)] (f 1))")))
   (is (empty? (lint! "(let [f #(apply println % %&)] (f 1 2 3 4 5 6))")))
-  (is (empty? (lint! "(fn ^:static meta [x] (if (instance? clojure.lang.IMeta x)
+  (is (empty? (lint! "(fn ^:static meta [x] (when (instance? clojure.lang.IMeta x)
                        (. ^clojure.lang.IMeta x (meta))))")))
   (is (empty? (lint! "(doseq [fn [inc]] (fn 1))")))
   (is (empty?
