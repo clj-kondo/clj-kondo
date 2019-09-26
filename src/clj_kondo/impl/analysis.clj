8e0828bbfc055a07acf52124eb54a1e8a9ce9ac8
Michiel Borkent <michielborkent@gmail.com>
2019-09-26 20:33:35 +0300

    [#476] fix for analysis export when CLJS has string require

--- src/clj_kondo/impl/analysis.clj
+++ src/clj_kondo/impl/analysis.clj
@@ -4,26 +4,25 @@
   (:refer-clojure :exclude [ns-name])
   (:require [clj-kondo.impl.utils :refer [assoc-some select-some]]))
 
-;; {:added "1.2", :ns #object[clojure.lang.Namespace 0x79c7532f "clojure.core"], :name inc, :file "clojure/core.clj", :column 1, :line 922, :arglists ([x]), :doc "Returns a number one greater than num. Does not auto-promote\n  longs, will throw on overflow. See also: inc'", :inline #object[clojure.core$inc__inliner__5536 0x64b73e7a "clojure.core$inc__inliner__5536@64b73e7a"]}
-
 (defn reg-usage! [{:keys [analysis] :as _ctx}
                   filename row col from-ns to-ns var-name arity lang metadata]
-  (swap! analysis update :var-usages conj
-         (assoc-some
-             (merge
-              {:filename filename
-               :row row
-               :col col
-               :from from-ns
-               :to to-ns
-               :name var-name}
-              (select-some metadata
-                           [:private :macro
-                            :fixed-arities
-                            :var-args-min-arity
-                            :deprecated]))
-             :arity arity
-             :lang lang)))
+  (let [to-ns (or (some-> to-ns meta :raw-name) to-ns)]
+    (swap! analysis update :var-usages conj
+           (assoc-some
+            (merge
+             {:filename filename
+              :row row
+              :col col
+              :from from-ns
+              :to to-ns
+              :name var-name}
+             (select-some metadata
+                          [:private :macro
+                           :fixed-arities
+                           :var-args-min-arity
+                           :deprecated]))
+            :arity arity
+            :lang lang))))
 
 (defn reg-var! [{:keys [:analysis :base-lang :lang] :as _ctx}
                 filename row col ns name attrs]
@@ -53,12 +52,13 @@
 
 (defn reg-namespace-usage! [{:keys [:analysis :base-lang :lang] :as _ctx}
                             filename row col from-ns to-ns alias]
-  (swap! analysis update :namespace-usages conj
-         (assoc-some
-          {:filename filename
-           :row row
-           :col col
-           :from from-ns
-           :to to-ns}
-          :lang (when (= :cljc base-lang) lang)
-          :alias alias)))
+  (let [to-ns (or (some-> to-ns meta :raw-name) to-ns)]
+    (swap! analysis update :namespace-usages conj
+           (assoc-some
+            {:filename filename
+             :row row
+             :col col
+             :from from-ns
+             :to to-ns}
+            :lang (when (= :cljc base-lang) lang)
+            :alias alias))))
