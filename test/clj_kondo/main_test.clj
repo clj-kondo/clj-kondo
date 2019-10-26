59f8c3b671b577ff003bb974e4c2b21ea6108968
Michiel Borkent <michielborkent@gmail.com>
2019-10-26 10:50:12 +0200

    [#550] ignore reader tag in metadata

--- test/clj_kondo/main_test.clj
+++ test/clj_kondo/main_test.clj
@@ -1971,6 +1971,8 @@
   (is (empty? (lint! "(import my.ns.Obj) (Obj.)"
                      {:linters {:unresolved-symbol {:level :error}}})))
   (is (empty? (lint! (io/file "project.clj")
+                     {:linters {:unresolved-symbol {:level :error}}})))
+  (is (empty? (lint! "^{:a #js [1 2 3]} [1 2 3]"
                      {:linters {:unresolved-symbol {:level :error}}}))))
 
 (deftest deftest-test
