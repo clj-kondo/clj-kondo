14392db2985706f2412516a0dfb5a9accd3947b5
Michiel Borkent <michielborkent@gmail.com>
2019-11-20 23:28:29 +0100

    windows

--- script/compile.bat
+++ script/compile.bat
@@ -18,15 +18,16 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 Rem the --no-server option is not supported in GraalVM Windows.
 call %GRAALVM_HOME%\bin\native-image.cmd ^
-  -jar target/clj-kondo-%CLJ_KONDO_VERSION%-standalone.jar ^
+  "-jar" "target/clj-kondo-%CLJ_KONDO_VERSION%-standalone.jar" ^
   "-H:Name=clj-kondo" ^
   "-H:+ReportExceptionStackTraces" ^
   "-J-Dclojure.spec.skip-macros=true" ^
   "-J-Dclojure.compiler.direct-linking=true" ^
-  "-H:IncludeResources=clj_kondo\impl\cache\built_in\.*" ^
+  "-H:IncludeResources=clj_kondo\impl\cache\built_in\*.*" ^
   "-H:ReflectionConfigurationFiles=reflection.json" ^
   "--initialize-at-build-time"  ^
   "-H:Log=registerResource:" ^
+  "--no-fallback" ^
   "--verbose" ^
   "-J-Xmx3g"
 if %errorlevel% neq 0 exit /b %errorlevel%
