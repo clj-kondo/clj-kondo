34385a64d1480230ac432c13bd670833d27115e3
Michiel Borkent <michielborkent@gmail.com>
2019-11-24 21:22:54 +0100

    dude

--- script/test.bat
+++ script/test.bat
@@ -1,19 +1,20 @@
 @echo off
 
+rem clojure doesn't work yet
 rem populate mvn cache for extract tests
-clojure -Sdeps '{:deps {org.clojure/clojure {:mvn/version ""1.9.0""}}}' -Spath > NUL
-clojure -Sdeps '{:deps {org.clojure/clojure {:mvn/version ""1.10.1""}}}' -Spath > NUL
-clojure -Sdeps '{:deps {org.clojure/clojurescript {:mvn/version ""1.10.520""}}}' -Spath > NUL
+rem clojure -Sdeps '{:deps {org.clojure/clojure {:mvn/version ""1.9.0""}}}' -Spath > NUL
+rem clojure -Sdeps '{:deps {org.clojure/clojure {:mvn/version ""1.10.1""}}}' -Spath > NUL
+rem clojure -Sdeps '{:deps {org.clojure/clojurescript {:mvn/version ""1.10.520""}}}' -Spath > NUL
 
 if "%CLJ_KONDO_TEST_ENV%"=="native" (
-  clojure -A:test
+  rem clojure -A:test
 )
 else (
   echo "Testing with Clojure 1.9.0"
-  clojure -A:clojure-1.9.0:test
+  rem clojure -A:clojure-1.9.0:test
   lein with-profiles +clojure-1.9.0 do clean, test
 
   echo "Testing with Clojure 1.10.1"
-  clojure -A:clojure-1.10.1:test
+  rem clojure -A:clojure-1.10.1:test
   lein with-profiles +clojure-1.10.1 do clean, test
 )
