23b57feb09877c07698cec42c26937db7ddde9bd
Michiel Borkent <michielborkent@gmail.com>
2019-11-24 21:23:56 +0100

    wip

--- script/test.bat
+++ script/test.bat
@@ -12,9 +12,9 @@ if "%CLJ_KONDO_TEST_ENV%"=="native" (
 else (
   echo "Testing with Clojure 1.9.0"
   rem clojure -A:clojure-1.9.0:test
-  lein with-profiles +clojure-1.9.0 do clean, test
+  call lein with-profiles +clojure-1.9.0 do clean, test
 
   echo "Testing with Clojure 1.10.1"
   rem clojure -A:clojure-1.10.1:test
-  lein with-profiles +clojure-1.10.1 do clean, test
+  call lein with-profiles +clojure-1.10.1 do clean, test
 )
