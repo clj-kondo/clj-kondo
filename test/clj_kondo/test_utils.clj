594a0236c81815ad27837507145fbd9215000e9b
Michiel Borkent <michielborkent@gmail.com>
2019-11-24 18:34:20 +0100

    Normalize filename in tests

--- test/clj_kondo/test_utils.clj
+++ test/clj_kondo/test_utils.clj
@@ -9,6 +9,9 @@
 
 (set! *warn-on-reflection* true)
 
+(defn normalize-filename [s]
+  (str/replace s "\\" "/"))
+
 (defn submap?
   "Is m1 a subset of m2? Taken from
   https://github.com/clojure/spec-alpha2, clojure.test-clojure.spec"
@@ -16,7 +19,10 @@
   (cond
     (and (map? m1) (map? m2))
     (every? (fn [[k v]] (and (contains? m2 k)
-                             (submap? v (get m2 k))))
+                             (if (identical? k :filename)
+                               (= (normalize-filename v)
+                                  (normalize-filename (get m2 k)))
+                               (submap? v (get m2 k)))))
             m1)
     (instance? java.util.regex.Pattern m1)
     (re-find m1 m2)
