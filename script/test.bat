71b35be348e1850c9988c5a4fbb97bf50e0b2798
Michiel Borkent <michielborkent@gmail.com>
2019-11-24 20:35:41 +0100

    bat

--- script/test.bat
+++ script/test.bat
@@ -1,19 +1,19 @@
 @echo off
 
-# populate mvn cache for extract tests
+rem populate mvn cache for extract tests
 clojure -Sdeps '{:deps {org.clojure/clojure {:mvn/version "1.9.0"}}}' -Spath > /dev/null
 clojure -Sdeps '{:deps {org.clojure/clojure {:mvn/version "1.10.1"}}}' -Spath > /dev/null
 clojure -Sdeps '{:deps {org.clojure/clojurescript {:mvn/version "1.10.520"}}}' -Spath > /dev/null
 
 if "%CLJ_KONDO_TEST_ENV%"=="native" (
-        clojure -A:test
+  clojure -A:test
 )
 else (
-    echo "Testing with Clojure 1.9.0"
-    clojure -A:clojure-1.9.0:test
-    lein with-profiles +clojure-1.9.0 do clean, test
+  echo "Testing with Clojure 1.9.0"
+  clojure -A:clojure-1.9.0:test
+  lein with-profiles +clojure-1.9.0 do clean, test
 
-    echo "Testing with Clojure 1.10.1"
-    clojure -A:clojure-1.10.1:test
-    lein with-profiles +clojure-1.10.1 do clean, test
+  echo "Testing with Clojure 1.10.1"
+  clojure -A:clojure-1.10.1:test
+  lein with-profiles +clojure-1.10.1 do clean, test
 )
