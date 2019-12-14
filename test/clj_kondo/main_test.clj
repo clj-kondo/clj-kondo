8d8902bfd52bf3a225aef747ad246ec34252cc54
Michiel Borkent <michielborkent@gmail.com>
2019-12-14 11:03:50 +0100

    wip

--- test/clj_kondo/main_test.clj
+++ test/clj_kondo/main_test.clj
@@ -1783,9 +1783,7 @@
   (is (empty? (lint! "(import my.ns.Obj) (Obj.)"
                      {:linters {:unresolved-symbol {:level :error}}})))
   (is (empty? (lint! (io/file "project.clj")
-                     {:linters {:unresolved-symbol {:level :error}
-                                ;; TODO:
-                                :unresolved-namespace {:level :off}}})))
+                     {:linters {:unresolved-symbol {:level :error}}})))
   (is (empty? (lint! "^{:a #js [1 2 3]} [1 2 3]"
                      {:linters {:unresolved-symbol {:level :error}}})))
   (is (empty? (lint! (io/file "corpus" "metadata.clj")
--- test/clj_kondo/unresolved_namespace_test.clj
+++ test/clj_kondo/unresolved_namespace_test.clj
@@ -2,8 +2,7 @@
   (:require
    [clj-kondo.test-utils :refer [lint! assert-submaps]]
    [clojure.java.io :as io]
-   [clojure.test :as t :refer [deftest is testing]]
-   [clojure.edn :as edn]))
+   [clojure.test :as t :refer [deftest is testing]]))
 
 (deftest unresolved-namespace-test
   (assert-submaps
@@ -11,8 +10,7 @@
       :message "Unresolved namespace clojure.string. Are you missing a require?"})
    (lint! "(clojure.string/includes? \"foo\" \"o\")"))
   ;; avoiding false positives
-  ;; TODO:
-  #_(is (empty? (lint! (io/file "project.clj"))))
+  (is (empty? (lint! (io/file "project.clj"))))
   (is (empty? (lint! "js/foo" "--lang" "cljs")))
   (is (empty? (lint! "goog/foo" "--lang" "cljs")))
   (is (empty? (lint! "(java.lang.Foo/Bar)")))
