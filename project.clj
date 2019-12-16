8cd4cdeb8bdbbbc7fc0a7f8774316bc1710f7d77
Michiel Borkent <michielborkent@gmail.com>
2019-12-16 12:08:40 +0100

    Version bump

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 ;; To change dependencies, update deps.edn and run script/update-project.clj.
 ;; To change other things, edit project.template.clj and run script/update-project.clj.
 
-(defproject clj-kondo "2019.12.14"
+(defproject clj-kondo "2019.12.17-SNAPSHOT"
   :description "Tidy your code with clj-kondo."
   :url "https://github.com/borkdude/clj-kondo"
   :scm {:name "git"
