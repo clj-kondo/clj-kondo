278e7c10b6f7ad1c3caba271ec1406c873d60b08
Michiel Borkent <michielborkent@gmail.com>
2020-02-15 11:10:39 +0100

    v2020.02.15

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 ;; To change dependencies, update deps.edn and run script/update-project.clj.
 ;; To change other things, edit project.template.clj and run script/update-project.clj.
 
-(defproject clj-kondo "2020.01.28-SNAPSHOT"
+(defproject clj-kondo "2020.02.15"
   :description "Tidy your code with clj-kondo."
   :url "https://github.com/borkdude/clj-kondo"
   :scm {:name "git"
