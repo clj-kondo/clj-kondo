a26128a373842817dbf9a6e151c6c86f1ddebf72
Sean Corfield <sean@corfield.org>
2020-03-27 15:30:00 -0700

    Address feedback on #823

--- src/clj_kondo/impl/analyzer/jdbc.clj
+++ src/clj_kondo/impl/analyzer/jdbc.clj
@@ -1,7 +1,7 @@
 (ns clj-kondo.impl.analyzer.jdbc
   {:no-doc true}
   (:require
-   [clj-kondo.impl.analyzer.common :refer [analyze-children
+   [clj-kondo.impl.analyzer.common :refer [analyze-expression**
                                            analyze-like-let]]
    [clj-kondo.impl.findings :as findings]
    [clj-kondo.impl.utils :as utils :refer [node->line]]))
@@ -18,8 +18,9 @@
   [{:keys [filename callstack] :as ctx} expr]
   (let [call (-> callstack first second)
         [f bindings & body] (:children expr)
-        [sym db-expr opts]  (:children bindings)]
-    (when-not (<= 2 (count (:children bindings)) 3)
+        binding-forms (:children bindings)
+        [sym db-expr opts] binding-forms]
+    (when-not (<= 2 (count binding-forms) 3)
       (findings/reg-finding!
        ctx
        (node->line filename bindings :error :syntax
@@ -29,12 +30,12 @@
        ctx
        (node->line filename sym :error :syntax
                    (format "%s binding form requires a symbol" call))))
-    (let [opts-analyzed (analyze-children ctx opts)
-          analyzed
-          (analyze-like-let ctx
-                            (assoc expr :children
-                                   (cons f
-                                         (cons (assoc bindings :children
-                                                      (list sym db-expr))
-                                               body))))]
+    (let [opts-analyzed (analyze-expression** ctx opts)
+          ;; a normal binding vector with just these two forms:
+          simple-binding (assoc bindings :children (list sym db-expr))
+          ;; reconstruct the expression with the normal binding instead:
+          expr-with-simple-binding
+          (assoc expr :children (concat [f simple-binding] body))
+          ;; now we can treat it like a regular let expression:
+          analyzed (analyze-like-let ctx expr-with-simple-binding)]
       (concat opts-analyzed (doall analyzed)))))
--- test/clj_kondo/jdbc_test.clj
+++ test/clj_kondo/jdbc_test.clj
@@ -1,8 +1,8 @@
 (ns clj-kondo.jdbc-test
   (:require
-   [clj-kondo.test-utils :refer [lint!]]
+   [clj-kondo.test-utils :refer [assert-submaps lint!]]
    [clojure.java.io :as io]
-   [clojure.test :as t :refer [deftest is testing]]
+   [clojure.test :refer [deftest is]]
    [missing.test.assertions]))
 
 (deftest next-jdbc-test
@@ -12,3 +12,14 @@
 (deftest clojure-java-jdbc-test
   (is (empty? (lint! (io/file "corpus" "jdbc" "cjj_test.clj")
                      {:linters {:unresolved-symbol {:level :error}}}))))
+
+(deftest detected-issues-test
+  (assert-submaps '({:file "corpus/jdbc/next.clj", :row 9, :col 24, :level :error,
+                     :message "with-transaction binding form requires exactly 2 or 3 forms"}
+                    {:file "corpus/jdbc/next.clj", :row 12, :col 24, :level :error,
+                     :message "with-transaction binding form requires exactly 2 or 3 forms"}
+                    {:file "corpus/jdbc/next.clj", :row 16, :col 3, :level :error,
+                     :message "with-transaction requires a vector for its binding"}
+                    {:file "corpus/jdbc/next.clj", :row 18, :col 25, :level :error,
+                     :message "with-transaction binding form requires a symbol"})
+                  (lint! (io/file "corpus" "jdbc" "next.clj"))))
