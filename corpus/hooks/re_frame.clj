1af2be8e5a3039cd32806c10978f6d30ca122fd4
Michiel Borkent <michielborkent@gmail.com>
2020-06-20 13:14:27 +0200

    wip

--- corpus/hooks/re_frame.clj
+++ corpus/hooks/re_frame.clj
@@ -1,25 +1,7 @@
 (ns bar
   {:clj-kondo/config '{:linters {:re-frame/keyword {:level :warning}}
-                       :hooks {:analyze-call {re-frame.core/dispatch "
-
-(require '[clj-kondo.hooks-api :as api])
-
-(fn [{:keys [:node]}]
-  (let [sexpr (api/sexpr node)
-        event (second sexpr)]
-    (when-not (vector? event)
-      (throw (ex-info \"dispatch arg should be vector!\"
-               (or (meta (second (:children node))) {}))))
-
-    (when-not (qualified-keyword? (first event))
-      (let [{:keys [:row :col]} (some-> node :children second :children first meta)]
-        (api/reg-finding! {:message \"keyword should be fully qualified!\"
-                           :type :re-frame/keyword
-                           :row row
-                           :col col})))))"}}}}
+                       :hooks {:analyze-call {re-frame.core/dispatch hooks.re-frame/dispatch}}}}
   (:require [re-frame.core :as r :refer [dispatch]]))
 
 (dispatch 1)
 (dispatch [:foo 1])
-
-
