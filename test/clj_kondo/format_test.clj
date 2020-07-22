0ede4823100013dfd00e62364d52e4dbc4e59e11
Michiel Borkent <michielborkent@gmail.com>
2020-07-22 23:11:45 +0200

    wip

---
+++ test/clj_kondo/format_test.clj
@@ -0,0 +1,7 @@
+(ns clj-kondo.format-test
+  (:require
+   [clj-kondo.core :as clj-kondo]
+   [clj-kondo.test-utils :refer [assert-submaps]]
+   [clojure.edn :as edn]
+   [clojure.test :as t :refer [deftest is testing]]))
+
