144395c10465eb87c3b4916a9e5c95c8c22836a3
Michiel Borkent <michielborkent@gmail.com>
2020-07-24 11:10:27 +0200

    wip

--- test/clj_kondo/format_test.clj
+++ test/clj_kondo/format_test.clj
@@ -8,4 +8,9 @@
   (assert-submaps
    '({:file "<stdin>", :row 1, :col 1, :level :error, :message "Format string expects 2 arguments instead of 1."})
    (lint! "(format \"%s %s\" 1)"))
-  (is (empty? (lint! "(defn foo [x] (format x 1))"))))
+  ;; TODO: fix
+  (prn (lint! "(format \"%3$s %s %s %s\" 1 2 3)"))
+  (is (empty? (lint! "(defn foo [x] (format x 1))")))
+  (is (empty? (lint! "
+(ns foo {:clj-kondo/config '{:linters {:format {:level :off}}}})
+(format \"%s\" 1 2)"))))
