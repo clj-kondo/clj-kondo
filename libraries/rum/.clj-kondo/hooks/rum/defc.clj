ebb395eb1082bcb0805a04090c98bf459cc0ec9f
Karol Wójcik <karol.wojcik@tuta.io>
2020-08-07 09:39:19 +0200

    Provide impl for rum.core/defcs

--- libraries/rum/.clj-kondo/hooks/rum/defc.clj
+++ libraries/rum/.clj-kondo/hooks/rum/defc.clj
@@ -21,3 +21,30 @@
     ;; (prn (meta sexpr))
     ;; (prn expr)
     {:node new-node}))
+
+(defn defcs
+  [{:keys [node]}]
+  (let [args (rest (:children node))
+        component-name (first args)
+        args (next args)
+        body (loop [args* args
+                    mixins []]
+               (if (seq args*)
+                 (let [a (first args*)]
+                   (if (vector? (api/sexpr a))
+                     (cons a (concat mixins (rest args*)))
+                     (recur (rest args*)
+                            (conj mixins a))))
+                 args))
+        partial-state (api/list-node [(api/token-node 'partial)
+                                    (api/list-node (list* (api/token-node 'fn) body))
+                                    (api/token-node 'state)])
+        new-node (api/list-node
+                  [(api/token-node 'let)
+                   (api/vector-node
+                    [(api/token-node 'state) (api/token-node {})])
+                   (with-meta (api/list-node [(api/token-node 'def)
+                                              component-name
+                                              partial-state])
+                     (meta node))])]
+    {:node new-node}))
