b896c894b4f654c68bc5ae412d3b3b0358786df7
Michiel Borkent <michielborkent@gmail.com>
2020-09-03 19:47:54 +0200

    wip

--- test/clj_kondo/ignore_test.clj
+++ test/clj_kondo/ignore_test.clj
@@ -4,6 +4,10 @@
 
 (deftest ignore-test
   (is (empty? (lint! "#_:clj-kondo/ignore (inc :foo)"
+                     {:linters {:type-mismatch {:level :warning}}})))
+  (is (empty? (lint! "#_:clj-kondo/ignore (inc 1 2 3)"
+                     {:linters {:type-mismatch {:level :warning}}})))
+  (is (empty? (lint! "#_:clj-kondo/ignore (defn foo [] (inc 1 2 3))"
                      {:linters {:type-mismatch {:level :warning}}}))))
 
 ;; TODO: #_:clj-kondo/ignore x
