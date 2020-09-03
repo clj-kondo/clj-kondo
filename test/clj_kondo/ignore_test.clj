95777de1acaaba54ac033d89de901176f860e881
Michiel Borkent <michielborkent@gmail.com>
2020-09-03 18:31:08 +0200

    wip

--- test/clj_kondo/ignore_test.clj
+++ test/clj_kondo/ignore_test.clj
@@ -1,9 +1,10 @@
 (ns clj-kondo.ignore-test
-  (:require  [clojure.test :as t :refer [deftest is testing]]
-             [clj-kondo.test-utils :refer [lint! assert-submaps]]))
+  (:require [clj-kondo.test-utils :refer [lint!]]
+            [clojure.test :as t :refer [deftest is testing]]))
 
 (deftest ignore-test
   (is (empty? (lint! "#_:clj-kondo/ignore (inc :foo)"
                      {:linters {:type-mismatch {:level :warning}}}))))
 
-
+;; TODO: #_:clj-kondo/ignore x
+;; TODO: #_:clj-kondo/ignore (inc :foo 1 2 3)
