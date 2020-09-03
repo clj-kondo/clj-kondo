75810df5ea5d740af96246b9300c97843bb41031
Michiel Borkent <michielborkent@gmail.com>
2020-09-03 20:15:15 +0200

    wip

--- test/clj_kondo/ignore_test.clj
+++ test/clj_kondo/ignore_test.clj
@@ -10,7 +10,13 @@
   (is (empty? (lint! "#_:clj-kondo/ignore (defn foo [] (inc 1 2 3))"
                      {:linters {:type-mismatch {:level :warning}}})))
   (is (empty? (lint! "#_:clj-kondo/ignore (defn foo [] x)"
-                     {:linters {:unresolved-symbol {:level :error}}}))))
+                     {:linters {:unresolved-symbol {:level :error}}})))
+  (is (empty? (lint! "#_:clj-kondo/ignore (\"foo\")"
+                     {:linters {:not-a-function {:level :error}}})))
+  (is (empty? (lint! "#_:clj-kondo/ignore {:A}"
+                     {:syntax {:level :error}}))))
 
 ;; TODO: (let [#_:clj-kondo/ignore x 1])
+
+
 
