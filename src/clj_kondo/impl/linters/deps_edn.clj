46a0c0a621dec50806d8b2beff6464c3ea08932e
Michiel Borkent <michielborkent@gmail.com>
2020-11-02 22:12:16 +0100

    wip

--- src/clj_kondo/impl/linters/deps_edn.clj
+++ src/clj_kondo/impl/linters/deps_edn.clj
@@ -9,22 +9,33 @@
         vals (take-nth 2 (rest children))]
     (zipmap keys vals)))
 
-(defn lint-qualified-deps [ctx key-nodes]
-  (run! (fn [node]
-          (let [form (sexpr node)]
-            (when-not (qualified-symbol? form)
-              (findings/reg-finding! ctx
-                                     (node->line (:filename ctx)
-                                                 node
-                                                 :warning
-                                                 :deps.edn
-                                                 (format "Libs must be qualified, change %s => %<s/%<s" form))))))
-        key-nodes))
+(defn key-nodes [map-node]
+  (take-nth 2 (:children map-node)))
+
+(defn val-nodes [map-node]
+  (take-nth 2 (rest (:children map-node))))
+
+
+(defn lint-qualified-deps [ctx dep-map]
+  (let [keys (key-nodes dep-map)]
+    (run! (fn [node]
+            (let [form (sexpr node)]
+              (when-not (qualified-symbol? form)
+                (findings/reg-finding!
+                 ctx
+                 (node->line (:filename ctx)
+                             node
+                             :warning
+                             :deps.edn
+                             (format "Libs must be qualified, change %s => %<s/%<s" form))))))
+          keys)))
 
 (defn lint-deps-edn [ctx expr]
   (let [deps-edn (sexpr-keys expr)
-        deps (:deps deps-edn)
-        dep-keys (take-nth 2 (:children deps))
-        _ (lint-qualified-deps ctx dep-keys)]
+        _ (lint-qualified-deps ctx (:deps deps-edn))
+        aliases (:aliases deps-edn)
+        alias-maps (val-nodes aliases)
+        alias-maps (map sexpr-keys alias-maps)
+        extra-dep-maps (map :extra-deps alias-maps)]
+    (run! #(lint-qualified-deps ctx %) extra-dep-maps)
     nil))
-
