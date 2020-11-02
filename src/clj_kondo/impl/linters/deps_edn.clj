0394e3fc063edc0c4399992fad898145e73d2bd1
Michiel Borkent <michielborkent@gmail.com>
2020-11-02 22:19:37 +0100

    exclusions

--- src/clj_kondo/impl/linters/deps_edn.clj
+++ src/clj_kondo/impl/linters/deps_edn.clj
@@ -16,26 +16,29 @@
   (take-nth 2 (rest (:children map-node))))
 
 
-(defn lint-qualified-deps [ctx dep-map]
-  (let [keys (key-nodes dep-map)]
-    (run! (fn [node]
-            (let [form (sexpr node)]
-              (when-not (qualified-symbol? form)
-                (findings/reg-finding!
-                 ctx
-                 (node->line (:filename ctx)
-                             node
-                             :warning
-                             :deps.edn
-                             (format "Libs must be qualified, change %s => %<s/%<s" form))))))
-          keys)))
+(defn lint-qualified-deps [ctx nodes]
+  (run! (fn [node]
+          (let [form (sexpr node)]
+            (when-not (qualified-symbol? form)
+              (findings/reg-finding!
+               ctx
+               (node->line (:filename ctx)
+                           node
+                           :warning
+                           :deps.edn
+                           (format "Libs must be qualified, change %s => %<s/%<s" form))))))
+        nodes))
 
 (defn lint-deps-edn [ctx expr]
   (let [deps-edn (sexpr-keys expr)
-        _ (lint-qualified-deps ctx (:deps deps-edn))
+        _ (lint-qualified-deps ctx (-> deps-edn :deps key-nodes))
         aliases (:aliases deps-edn)
         alias-maps (val-nodes aliases)
         alias-maps (map sexpr-keys alias-maps)
-        extra-dep-maps (map :extra-deps alias-maps)]
-    (run! #(lint-qualified-deps ctx %) extra-dep-maps)
+        extra-dep-maps (map :extra-deps alias-maps)
+        extra-dep-map-vals (mapcat val-nodes extra-dep-maps)
+        extra-dep-map-vals (map sexpr-keys extra-dep-map-vals)
+        exclusions (map (comp :children :exclusions) extra-dep-map-vals)]
+    (run! #(lint-qualified-deps ctx (key-nodes %)) extra-dep-maps)
+    (run! #(lint-qualified-deps ctx %) exclusions)
     nil))
