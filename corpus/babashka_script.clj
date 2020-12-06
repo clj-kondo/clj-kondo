ed682a86dcb0d7f6dd6163eb78b678a80e3751f6
Michiel Borkent <michielborkent@gmail.com>
2020-12-06 12:46:12 +0100

    [#1089] test

--- corpus/babashka_script.clj
+++ corpus/babashka_script.clj
@@ -1,15 +1,17 @@
 #!/usr/bin/env bb
 
-(require '[babashka.process :as p])
+(require '[babashka.process :refer [$ check]])
 
 (def out-page (str "gh-pages/"
                    (or (System/getenv "BABASHKA_BOOK_MAIN")
                        "master")
                    ".html"))
 
-@(p/$ asciidoctor src/book.adoc -o ~out-page -a docinfo=shared)
+(-> ($ asciidoctor src/book.adoc -o ~out-page -a docinfo=shared)
+    check)
 
 (binding [*out* *err*]
   (println "Done writing to" out-page))
 
-(p/$ ~x) ;; unresolved
+(-> ($ ~x) ;; unresolved
+    check)
