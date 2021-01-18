f02127a23eea7c7474ac970de63b9f8b479677bb
Michiel Borkent <michielborkent@gmail.com>
2021-01-18 23:21:26 +0100

    wip

--- test/clj_kondo/unresolved_var_test.clj
+++ test/clj_kondo/unresolved_var_test.clj
@@ -7,12 +7,12 @@
   (assert-submaps
    '({:file "<stdin>", :row 1, :col 35, :level :error, :message "No such var: set/onion"})
    (lint! "(require '[clojure.set :as set]) (set/onion) set/union"
-          '{:linters {:unresolved-symbol {:level :error}}}))
+          '{:linters {:unresolved-var {:level :error}}}))
   (assert-submaps
    '({:file "<stdin>", :row 1, :col 41, :level :error, :message "No such var: set/onion"})
    (lint! "(require '[clojure.set :as set]) (apply set/onion 1 2 3)"
-          '{:linters {:unresolved-symbol {:level :error}}}))
+          '{:linters {:unresolved-var {:level :error}}}))
   (is (empty?
        (lint! "(do 1 2) goog.global"
-              '{:linters {:unresolved-symbol {:level :error}}}
+              '{:linters {:unresolved-var {:level :error}}}
               "--lang" "cljs"))))
