ac2af769a632ac6fcf97b02cf62c8dba87de000d
Michiel Borkent <michielborkent@gmail.com>
2021-01-29 10:57:55 +0100

    wip

--- test/clj_kondo/core_match_test.clj
+++ test/clj_kondo/core_match_test.clj
@@ -86,4 +86,12 @@
          [{:a 1 :b 1}] :a1
          [{:c 3 :d _ :e 4}] :a2
          :else nil))
+"))))
+  (testing ":or pattern"
+    (is (empty? (lint!! "
+(let [x 4 y 6 z 9]
+  (match [x y z]
+         [(:or 1 2 3) _ _] :a0
+         [4 (:or 5 6 7) _] :a1
+         :else []))
 ")))))
