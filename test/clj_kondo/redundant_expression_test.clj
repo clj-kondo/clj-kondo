6baee5782d95d148075d89261e44457440831fde
Michiel Borkent <michielborkent@gmail.com>
2021-02-22 10:49:39 +0100

    Add test

---
+++ test/clj_kondo/redundant_expression_test.clj
@@ -0,0 +1,9 @@
+(ns clj-kondo.redundant-expression-test
+  (:require
+   [clj-kondo.test-utils :refer [lint! assert-submaps]]
+   [clojure.test :refer [deftest is testing]]))
+
+(deftest shadowed-var-test
+  (assert-submaps
+   '({:file "<stdin>", :row 1, :col 5, :level :warning, :message "Redundant expression: 1"})
+   (lint! "(do 1 2) ")))
