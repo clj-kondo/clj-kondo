4f00574e5e8cb81d29277c937a1e6a7f0467fcfd
Michiel Borkent <michielborkent@gmail.com>
2021-03-03 10:07:59 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 ;; To change dependencies, update deps.edn and run script/update-project.clj.
 ;; To change other things, edit project.template.clj and run script/update-project.clj.
 
-(defproject clj-kondo "2021.03.03"
+(defproject clj-kondo "2021.03.04-SNAPSHOT"
   :description "A linter for Clojure that sparks joy."
   :url "https://github.com/clj-kondo/clj-kondo"
   :scm {:name "git"
