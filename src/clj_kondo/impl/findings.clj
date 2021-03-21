d9b1eb2d3e14af16197985581c37a6fddd292800
Michiel Borkent <michielborkent@gmail.com>
2021-03-21 11:26:57 +0100

    [#1210] Fix crash with :clj-kondo/ignore in combination with :rename (#1225)
    * [#1210] Fix crash with :clj-kondo/ignore
    
    * Update diff script

--- src/clj_kondo/impl/findings.clj
+++ src/clj_kondo/impl/findings.clj
@@ -15,31 +15,32 @@
         filename (:filename m)
         lang (:lang ctx)
         row (:row m)]
-    (when-let [ignores (get-in ignores [filename lang])]
-      (loop [ignores ignores]
-        (when ignores
-          (let [ignore (first ignores)
-                ignore-row (:row ignore)]
-            (if (> ignore-row row)
-              ;; since ignores are sorted on row (and col) we can skip the rest of the checking here
-              false
-              ;; (>= row ignore row) is true from here
-              (if (or
-                   (> row ignore-row)
-                   ;; row and ignore-row are equal, so the col of the
-                   ;; finding has to be before the col of the ignore
-                   (>= (:col m) (:col ignore)))
-                (let [ignore-end-row (:end-row ignore)
-                      end-col (:end-col m)]
-                  (if (and end-col ;; if there is no end-col our finding location is incomplete...
-                           (or (< row ignore-end-row)
-                               (and (= row ignore-end-row)
-                                    (<= (:end-col m) (:end-col ignore)))))
-                    (if (ignore-match? (:ignore ignore) tp)
-                      true
-                      (recur (next ignores)))
-                    (recur (next ignores))))
-                (recur (next ignores))))))))))
+    (when row
+      (when-let [ignores (get-in ignores [filename lang])]
+        (loop [ignores ignores]
+          (when ignores
+            (let [ignore (first ignores)
+                  ignore-row (:row ignore)]
+              (if (> ignore-row row)
+                ;; since ignores are sorted on row (and col) we can skip the rest of the checking here
+                false
+                ;; (>= row ignore row) is true from here
+                (if (or
+                     (> row ignore-row)
+                     ;; row and ignore-row are equal, so the col of the
+                     ;; finding has to be before the col of the ignore
+                     (>= (:col m) (:col ignore)))
+                  (let [ignore-end-row (:end-row ignore)
+                        end-col (:end-col m)]
+                    (if (and end-col ;; if there is no end-col our finding location is incomplete...
+                             (or (< row ignore-end-row)
+                                 (and (= row ignore-end-row)
+                                      (<= (:end-col m) (:end-col ignore)))))
+                      (if (ignore-match? (:ignore ignore) tp)
+                        true
+                        (recur (next ignores)))
+                      (recur (next ignores))))
+                  (recur (next ignores)))))))))))
 
 (defn reg-finding! [ctx m]
   (let [dependencies (:dependencies ctx)
