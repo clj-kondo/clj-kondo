58b9093ab238dd7a3c7c1d062ad3f37007efcfee
Michiel Borkent <michielborkent@gmail.com>
2021-04-21 11:59:26 +0200

    Deploy artifacts to Github releases

---
+++ script/clj_kondo/release_artifact.clj
@@ -0,0 +1,24 @@
+(ns clj-kondo.release-artifact
+  (:require [borkdude.gh-release-artifact :as ghr]
+            [clojure.java.shell :refer [sh]]
+            [clojure.string :as str]))
+
+(defn current-branch []
+  (-> (sh "git" "rev-parse" "--abbrev-ref" "HEAD")
+      :out
+      str/trim))
+
+(defn release [& args]
+  (let [current-version (-> (slurp "resources/CLJ_KONDO_VERSION")
+                            str/trim)
+        ght (System/getenv "GITHUB_TOKEN")
+        file (first args)
+        branch (current-branch)]
+    (if (and ght (contains? #{"master" "main"} branch))
+      (do (assert file "File name must be provided")
+          (ghr/overwrite-asset {:org "clj-kondo"
+                                :repo "clj-kondo"
+                                :file file
+                                :tag (str "v" current-version)}))
+      (println "Skipping release artifact (no GITHUB_TOKEN or not on main branch)"))
+    nil))
