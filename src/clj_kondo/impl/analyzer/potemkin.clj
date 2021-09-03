13066e47b8418072edc15a853164a2ef73ddc75a
Michiel Borkent <michielborkent@gmail.com>
2021-09-03 22:58:04 +0200

    [#1370] Simplify code

--- src/clj_kondo/impl/analyzer/potemkin.clj
+++ src/clj_kondo/impl/analyzer/potemkin.clj
@@ -1,8 +1,7 @@
 (ns clj-kondo.impl.analyzer.potemkin
   {:no-doc true}
   (:refer-clojure :exclude [ns-name])
-  (:require [clj-kondo.impl.namespace :as namespace]
-            [clj-kondo.impl.utils :refer [token-node]]))
+  (:require [clj-kondo.impl.namespace :as namespace]))
 
 (defn analyze-import-vars [ctx expr]
   (let [ns-name (-> ctx :ns :name)
@@ -10,16 +9,16 @@
     [{:type :import-vars
       :used-namespaces
       (for [g import-groups
-            :let [fqs-import? (qualified-ident? (:value g))
+            :let [gval (:value g)
+                  fqs-import? (and gval (qualified-symbol? gval))
+                  gchildren (:children g)
                   imported-ns (if fqs-import?
-                                (token-node (symbol (namespace (:value g))))
-                                (first (:children g)))
+                                (symbol (namespace gval))
+                                (:value (first gchildren)))
                   imported-vars (if fqs-import?
-                                  [(token-node (symbol (name (:value g))))]
-                                  (rest (:children g)))
-                  imported-ns-sym (:value imported-ns)]]
+                                  [(symbol (name gval))]
+                                  (map :value (rest gchildren)))]]
         (do (doseq [iv imported-vars]
-              (let [iv-sym (:value iv)]
-                (namespace/reg-var! ctx ns-name iv-sym expr {:imported-ns imported-ns-sym
-                                                             :imported-var iv-sym})))
-            imported-ns-sym))}]))
+              (namespace/reg-var! ctx ns-name iv expr {:imported-ns imported-ns
+                                                       :imported-var iv}))
+            imported-ns))}]))
