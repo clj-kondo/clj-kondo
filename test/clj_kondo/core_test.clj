a31362c15180716e95586a6d70422d313bf308a0
Eric Dallo <ericdallo06@hotmail.com>
2021-10-09 15:24:20 -0300

    Create failing tests

--- test/clj_kondo/core_test.clj
+++ test/clj_kondo/core_test.clj
@@ -90,10 +90,11 @@
            {:lint   ["-"]}))]
     (is (edn/read-string (pr-str findings)))))
 
-(defn custom-linter [code reg-callback]
+(defn custom-linter [code lang reg-callback]
   (with-in-str code
     (clj-kondo/run!
      {:lint   ["-"]
+      :lang lang
       :config {:linters {:org.acme/forbidden-var {:level :error}}
                :output {:analysis true}}
       :custom-lint-fn (fn [{:keys [analysis reg-finding!]}]
@@ -108,12 +109,18 @@
 
 (deftest custom-lint-fn-test
   (testing "custom-lint reg a new finding and reg-finding! return the new finding"
-    (let [res (custom-linter "(eval '(+ 1 2 3))" #(is %))]
+    (let [res (custom-linter "(eval '(+ 1 2 3))" :clj #(is %))]
       (is (= [{:filename "<stdin>", :row 1, :col 1, :end-row 1, :end-col 6,
                :type :org.acme/forbidden-var, :level :error}]
              (:findings res)))))
-  (testing "ignore hints return nil during reg-finding!"
-    (let [res (custom-linter "#_:clj-kondo/ignore (eval '(+ 1 2 3))" #(is (not %)))]
+  (testing "ignore hints return nil during reg-finding! for clj files"
+    (let [res (custom-linter "#_:clj-kondo/ignore (eval '(+ 1 2 3))" :clj #(is (not %)))]
+      (is (empty? (:findings res)))))
+  (testing "ignore hints return nil during reg-finding! for clj files"
+    (let [res (custom-linter "#_:clj-kondo/ignore (eval '(+ 1 2 3))" :cljs #(is (not %)))]
+      (is (empty? (:findings res)))))
+  (testing "ignore hints return nil during reg-finding! for cljc files"
+    (let [res (custom-linter "#_:clj-kondo/ignore (eval '(+ 1 2 3))" :cljc #(is (not %)))]
       (is (empty? (:findings res))))))
 
 ;;;; Scratch
