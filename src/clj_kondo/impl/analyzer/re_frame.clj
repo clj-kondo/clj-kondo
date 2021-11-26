da98936dbd34afbb83e0e5d8fd691a5e423641b1
Michiel Borkent <michielborkent@gmail.com>
2021-11-26 16:33:30 +0100

    update comment example [skip ci]

--- src/clj_kondo/impl/analyzer/re_frame.clj
+++ src/clj_kondo/impl/analyzer/re_frame.clj
@@ -30,11 +30,13 @@
 (comment
   (require '[clj-kondo.core :as clj-kondo])
   (-> (with-in-str "(require '[re-frame.core :as re-frame]) (re-frame/reg-sub ::foo (fn [_]))"
-        (clj-kondo/run! {:lang :cljs :lint ["-"] :config {:output {:analysis {:keywords true}}}}))
+        (clj-kondo/run! {:lang :cljs :lint ["-"] :config {:output {:analysis {:context true
+                                                                              :keywords true}}}}))
       :analysis :keywords)
 
   (-> (with-in-str "(require '[re-frame.core :as re-frame]) (re-frame/reg-sub ::foo (fn [x] (inc x)))"
-        (clj-kondo/run! {:lang :cljs :lint ["-"] :config {:output {:analysis {:keywords true}}}}))
+        (clj-kondo/run! {:lang :cljs :lint ["-"] :config {:output {:analysis {:context true
+                                                                              :keywords true}}}}))
       :analysis :var-usages)
 
   )
