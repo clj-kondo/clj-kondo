a5e5d8238e259a0bd8d3543c628b1c990dc0893f
Michiel Borkent <michielborkent@gmail.com>
2021-12-13 23:13:11 +0100

    clean

--- src/clj_kondo/impl/config.clj
+++ src/clj_kondo/impl/config.clj
@@ -35,7 +35,6 @@
                                ;;:exclude-destructured-keys-in-fn-args false
                                ;;:exclude-destructured-as false
                                ;;:exclude-defmulti-args false
-                               ;:exclude-incorrectly-marked-unused false
                                ,}
               :unsorted-required-namespaces {:level :off}
               :unused-namespace {:level :warning
