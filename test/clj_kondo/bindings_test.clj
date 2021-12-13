4a3b1cf502db2b189e305b1fff508f78ca6d1e9e
Maximilian Karasz <mknoszlig@zeroton.in>
2021-12-13 21:38:54 +0100

    put tests into one namespace to mirror linter structure.

--- test/clj_kondo/bindings_test.clj
+++ test/clj_kondo/bindings_test.clj
@@ -1,4 +1,4 @@
-(ns clj-kondo.unused-bindings-test
+(ns clj-kondo.bindings-test
   (:require [clj-kondo.test-utils :refer [assert-submaps lint!]]
             [clojure.test :refer [deftest is testing]]
             [missing.test.assertions]))
@@ -15,19 +15,6 @@
       :message "unused binding x"})
    (lint! "(defn foo [x])"
           '{:linters {:unused-binding {:level :warning}}}))
-  (assert-submaps
-   '({:file "<stdin>",
-      :row 1,
-      :col 7,
-      :level :warning,
-      :message "used binding _x marked as unused."}
-     {:file "<stdin>",
-      :row 1,
-      :col 29,
-      :level :warning,
-      :message "used binding _c marked as unused."})
-   (lint! "(let [_x 0 {:keys [a b] :as _c} v]  [a b _x _c])"
-          '{:linters {:used-underscored-binding {:level :warning}}}))
   (assert-submaps
    '({:file "<stdin>",
       :row 1,
@@ -342,3 +329,23 @@
                                           :exclude-destructured-as true}}})))))
 
 
+(deftest used-underscored-binding-test
+  (assert-submaps
+   '({:file "<stdin>",
+      :row 1,
+      :col 7,
+      :level :warning,
+      :message "Using binding marked as unused: _x"}
+     {:file "<stdin>",
+      :row 1,
+      :col 29,
+      :level :warning,
+      :message "Using binding marked as unused: _c"})
+   (lint! "(let [_x 0 {:keys [a b] :as _c} v]  [a b _x _c])"
+          '{:linters {:used-underscored-binding {:level :warning}}}))
+  (is (empty?  (lint! "(let [_x 0 {:keys [a b] :as _c} v]  [a b _x _c])"
+                      '{:linters {:used-underscored-binding {:level :off}}})))
+  (is (empty? (lint! "(doto (Object.) (.method))"
+                     '{:linters {:used-underscored-binding {:level :warning}}})))
+  (is (empty? (lint! "(let [_ 1] _)"
+                     '{:linters {:used-underscored-binding {:level :warning}}}))))
