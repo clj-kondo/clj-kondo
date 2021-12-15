01ec758c4492ea57244f4b58426ca3bbeb7e0a3a
Michiel Borkent <michielborkent@gmail.com>
2021-12-15 16:17:54 +0100

    finish

--- test/clj_kondo/recur_test.clj
+++ test/clj_kondo/recur_test.clj
@@ -8,8 +8,18 @@
    {:unexpected-recur {:level :error}}})
 
 (deftest unexpected-recur
-  (testing "all linters with def"
+  (testing "recur doesn't correspond to function or loop"
     (assert-submaps
      '({:file "<stdin>", :row 1, :col 1, :level :error, :message "Unexpected usage of recur."})
-     (lint! "(recur)" linter-config))))
+     (lint! "(recur)" linter-config)))
+  (testing "recur doesn't correspond to function or loop"
+    (assert-submaps
+     '({:file "<stdin>", :row 1, :col 8, :level :error, :message "Recur can only be used in tail position."})
+     (lint! "(fn [] (recur) 1)" linter-config)))
+  (testing "recur corresponds to function or loop"
+    (is (empty? (lint! "(fn [] (recur))" linter-config)))
+    (is (empty? (lint! "(loop [] (recur))" linter-config))))
+  (testing "recur is in tail position"
+    (is (empty? (lint! "(fn [] (if true (recur) 3))" linter-config)))
+    (is (empty? (lint! "(fn [x] (case x 1 (recur (inc x)) 2 :the-end))" linter-config)))))
 
