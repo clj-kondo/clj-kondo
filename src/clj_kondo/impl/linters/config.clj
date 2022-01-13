dd71916f6805e65e4ea7121a26a3663707d4ea49
Michiel Borkent <michielborkent@gmail.com>
2022-01-13 14:47:46 +0100

    another linter

--- src/clj_kondo/impl/linters/config.clj
+++ src/clj_kondo/impl/linters/config.clj
@@ -7,25 +7,37 @@
 
 (def expected-linter-keys (set (keys (:linters default-config))))
 
-(defn lint-linters [ctx node-map]
+(defn lint-linters [ctx node-map config-map]
   (doseq [key-node (keys node-map)]
     (let [k (sexpr key-node)]
       (when (and
              (simple-keyword? k)
-             (not(contains? expected-linter-keys k)))
+             (not (contains? expected-linter-keys k)))
         (findings/reg-finding!
          ctx
          (node->line (:filename ctx)
                      key-node
                      :clj-kondo-config
-                     (str "Unexpected linter name: " k)))))))
+                     (str "Unexpected linter name: " k))))))
+  (doseq [key-node (keys config-map)]
+    (let [k (sexpr key-node)]
+      (when (and
+             (simple-keyword? k)
+             (contains? expected-linter-keys k))
+        (findings/reg-finding!
+         ctx
+         (node->line (:filename ctx)
+                     key-node
+                     :clj-kondo-config
+                     (str "Linter config should go under :linters")))))))
 
 (defn lint-config [ctx expr]
   (try
     (let [config-edn (edn-utils/sexpr-keys expr)
+          config-map (edn-utils/node-map expr)
           linters (:linters config-edn)
           linters-map (edn-utils/node-map linters)
-          _ (lint-linters ctx linters-map)
+          _ (lint-linters ctx linters-map config-map)
           _lint-as (:lint-as config-edn)]
       :eastwood)
     ;; Due to ubiquitous use of sexpr, we're catching coercion errors here and let them slide.
--- test/clj_kondo/clj_kondo_config_test.clj
+++ test/clj_kondo/clj_kondo_config_test.clj
@@ -6,4 +6,7 @@
 (deftest unexpected-linter-name-test
   (assert-submaps
    '({:file ".clj-kondo/config.edn", :row 1, :col 12, :level :warning, :message "Unexpected linter name: :foo"})
-   (lint! "{:linters {:foo 1}}" "--filename" ".clj-kondo/config.edn")))
+   (lint! "{:linters {:foo 1}}" "--filename" ".clj-kondo/config.edn"))
+  (assert-submaps
+   '({:file ".clj-kondo/config.edn", :row 1, :col 2, :level :warning, :message "Linter config should go under :linters"})
+   (lint! "{:unresolved-symbol {}}" "--filename" ".clj-kondo/config.edn")))
