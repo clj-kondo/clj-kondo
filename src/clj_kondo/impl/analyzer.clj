d69406f5328b8f2972d6f273417773a931955993
Michiel Borkent <michielborkent@gmail.com>
2022-01-27 18:52:31 +0100

    remove redundant

--- src/clj_kondo/impl/analyzer.clj
+++ src/clj_kondo/impl/analyzer.clj
@@ -2171,9 +2171,6 @@
                ret-tag (assoc :ret ret-tag))]
     (when id
       (reg-call ctx call id))
-    ;; TODO: var-usage... this is not a good name and looks suspicious
-    ;; can we do it another way?
-    (namespace/reg-var-usage! ctx ns-name call)
     (if m
       (with-meta (cons call analyzed)
         m)
