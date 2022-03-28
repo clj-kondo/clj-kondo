781e6f5a069b436ff4de09da93221669942a3cb1
Michiel Borkent <michielborkent@gmail.com>
2022-03-28 14:07:21 +0200

    minor [skip ci]

--- src/clj_kondo/impl/analysis/java.clj
+++ src/clj_kondo/impl/analysis/java.clj
@@ -19,43 +19,13 @@
       (str/replace ".class" "")
       (str/replace ".java" "")))
 
-#_(defn class->package
-  "Implementation by Marco Marini."
-  [class-file]
-  (with-open [dis (java.io.DataInputStream. (io/input-stream class-file))]
-    ;; skip first 8 bytes
-    (.readLong dis)
-    (let [constant-pool-count (dec (.readUnsignedShort dis))
-          counter (volatile! 0)
-          classes (volatile! {})
-          strings (volatile! {})]
-      (while (< @counter constant-pool-count)
-        (do
-          (case (.read dis)
-            1 (vswap! strings assoc @counter (.readUTF dis))
-            5 (do (.skipBytes dis 8) (swap! counter inc))
-            6 (do (.skipBytes dis 8) (swap! counter inc))
-            7 (vswap! classes assoc  @counter (.readShort dis))
-            8 (.readShort dis)
-            (.readInt dis))
-          (swap! counter inc)))
-      ;; skip access flags
-      (.readShort dis)
-      ;; (prn (get @classes (.readUnsignedShort dis)))
-      (clojure.string/join
-       "."
-       (butlast
-        (clojure.string/split
-         (get @strings (- (get @classes (- (.readUnsignedShort dis) 1)) 1))
-         #"/"))))))
-
 (defn class->class-name [class-file]
   (let [bytes (file->bytes class-file)
         ;; we use ASM for reading the fully qualified class name
         ;; hand-made solutions, if we ever want to get rid of ASM:
         ;; https://stackoverflow.com/questions/1649674/resolve-class-name-from-bytecode/1650442#comment115293993_1650442
         ;; https://stackoverflow.com/a/52332101/6264
-        ;; see class->package above
+        ;; Also see working example in Clojure https://gist.github.com/borkdude/d02dc3ff1d03d09351e768964983a46b
         rdr (new ClassReader ^bytes bytes)
         class-name (.getClassName rdr)
         class-name (str/replace class-name "/" ".")]
