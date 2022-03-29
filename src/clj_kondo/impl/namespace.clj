183bde2f3f7373a066a1fd4231d16b1a2bca31db
Michiel Borkent <michielborkent@gmail.com>
2022-03-29 11:33:40 +0200

    wip [skip ci]

--- src/clj_kondo/impl/namespace.clj
+++ src/clj_kondo/impl/namespace.clj
@@ -281,7 +281,8 @@
     (or (let [should-be-capital-letter-idx (inc i)]
           (and (> (.length s) should-be-capital-letter-idx)
                (Character/isUpperCase ^char (.charAt s should-be-capital-letter-idx))))
-        (str/includes? s "_"))))
+        ;; underscore but not in first position
+        (pos? (str/index-of s "_")))))
 
 (defn reg-unresolved-symbol!
   [ctx ns-sym sym {:keys [:base-lang :lang :config
@@ -566,10 +567,21 @@
                                               (when-not (contains? excluded name-sym)
                                                 k))
                                             (:refer-alls ns))]
-                  {:ns (or referred-all-ns :clj-kondo/unknown-namespace)
-                   :name name-sym
-                   :unresolved? true
-                   :clojure-excluded? clojure-excluded?}))))))))))
+                  (cond referred-all-ns
+                    {:ns (or referred-all-ns :clj-kondo/unknown-namespace)
+                     :name name-sym
+                     :unresolved? true
+                     :clojure-excluded? clojure-excluded?}
+                    (class-name? name-sym)
+                    {:ns :clj-kondo/unknown-namespace
+                     :name name-sym
+                     :unresolved? true
+                     :clojure-excluded? clojure-excluded?
+                     :interop? true}
+                    :else {:ns :clj-kondo/unknown-namespace
+                           :name name-sym
+                           :unresolved? true
+                           :clojure-excluded? clojure-excluded?})))))))))))
 
 ;;;; Scratch
 
