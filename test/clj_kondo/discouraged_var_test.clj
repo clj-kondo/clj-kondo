416597b09a1c55aed631f9d83d1c2648d36e36cb
Michiel Borkent <michielborkent@gmail.com>
2022-04-23 18:08:28 +0200

    fix #996: discouraged var

---
+++ test/clj_kondo/discouraged_var_test.clj
@@ -0,0 +1,15 @@
+(ns clj-kondo.discouraged-var-test
+  (:require
+   [clj-kondo.test-utils :refer [lint! assert-submaps]]
+   [clojure.test :as t :refer [deftest is testing]]))
+
+(deftest config-in-ns-test
+  (assert-submaps
+   '({:file "<stdin>", :row 1, :col 15, :level :warning, :message "Too slow"})
+   (lint! "(defn foo [x] (satisfies? Datafy x))"
+          '{:linters  {:discouraged-var {clojure.core/satisfies? {:message "Too slow"}}}}))
+  (assert-submaps
+   '({:file "<stdin>", :row 1, :col 15, :level :warning, :message "Too slow"})
+   (lint! "(defn foo [x] (satisfies? Datafy x))"
+          '{:ns-groups [{:pattern "(cljs|clojure).core" :name core}]
+            :linters  {:discouraged-var {core/satisfies? {:message "Too slow"}}}})))
