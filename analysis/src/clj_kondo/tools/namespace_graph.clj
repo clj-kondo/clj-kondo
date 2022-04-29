418d19944cb806b0f464573be291545a9c1b7ed1
Jacob Maine <jacob.maine@gmail.com>
2022-04-28 17:17:50 -0700

    Minimize analysis in tools

--- analysis/src/clj_kondo/tools/namespace_graph.clj
+++ analysis/src/clj_kondo/tools/namespace_graph.clj
@@ -5,7 +5,9 @@
 
 (defn -main [& paths]
   (let [analysis (:analysis (clj-kondo/run! {:lint paths
-                                             :config {:analysis true}}))
+                                             :config {:analysis {:var-usages false
+                                                                 :var-definitions {:shallow true}}}
+                                             :skip-lint true}))
         {:keys [:namespace-definitions :namespace-usages]} analysis
         nodes (map :name namespace-definitions)
         edges (map (juxt :from :to) namespace-usages)
