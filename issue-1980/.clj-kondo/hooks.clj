5e1e07a5b437cda04317e4c1f42ccf9f38276b30
Michiel Borkent <michielborkent@gmail.com>
2023-02-10 21:17:22 +0100

    #1980: suppress lint

--- issue-1980/.clj-kondo/hooks.clj
+++ issue-1980/.clj-kondo/hooks.clj
@@ -2,4 +2,5 @@
   (:require [clj-kondo.hooks-api :as api]))
 
 (defn my-print [{:keys [node]}]
-  {:node (api/list-node (list* (api/token-node 'clojure.core/println) (rest (:children node))) )})
+  {:node (with-meta (api/list-node (list* (api/token-node 'clojure.core/println) (rest (:children node))))
+           {:clj-kondo/ignore [:discouraged-var]})})
