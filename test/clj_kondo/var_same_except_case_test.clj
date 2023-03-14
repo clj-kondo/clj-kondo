20be3765271096d332c5016bfc828c044db2ed5c
Emlyn Corrin <emlyn.corrin@gmail.com>
2023-03-14 01:17:18 +0000

    Add some more tests

--- test/clj_kondo/var_same_except_case_test.clj
+++ test/clj_kondo/var_same_except_case_test.clj
@@ -9,7 +9,23 @@
       :message "Foo differs only in case from foo"})
    (lint! "(defn foo [] 1) (defn Foo [] 2)"
           '{:linters {:var-same-except-case {:level :error}}}))
+  (assert-submaps
+   '({:file "<stdin>", :row 1, :col 21, :level :error,
+      :message "Foo differs only in case from foo"})
+   (lint! "(defmacro foo [] 1) (definline Foo [] 2)"
+          '{:linters {:var-same-except-case {:level :error}}}))
+  (assert-submaps
+   '({:file "<stdin>", :row 1, :col 19, :level :error,
+      :message "Foo differs only in case from foo"})
+   (lint! "(deftype Foo [_]) (defn- foo [] 1)"
+          '{:linters {:var-same-except-case {:level :error}}}))
   ;; avoiding false positives
   (is (empty?
-       (lint! "(def foo 1) (def Foo 2)"
+       (lint! "(def leppard 1) (def Leppard 2)"
+              '{:linters {:var-same-except-case {:level :error}}})))
+  (is (empty?
+       (lint! "(defprotocol Foo (foo [_] 1))"
+              '{:linters {:var-same-except-case {:level :error}}})))
+  (is (empty?
+       (lint! "(definterface Foo (foo [] 1))"
               '{:linters {:var-same-except-case {:level :error}}}))))
