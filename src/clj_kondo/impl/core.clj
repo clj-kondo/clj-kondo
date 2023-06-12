d640e1650c08f9d1de80f617c991568740e00dab
Eric Dallo <ericdallo06@hotmail.com>
2023-06-12 08:26:11 -0300

    Consider cljd files as source files (#2106)
    * Consider cljd files as source files
    
    * Add tests

--- src/clj_kondo/impl/core.clj
+++ src/clj_kondo/impl/core.clj
@@ -191,7 +191,7 @@
 
 (defn source-file? [filename]
   (when-let [[_ ext] (re-find #"\.(\w+)$" filename)]
-    (one-of (keyword ext) [:clj :cljs :cljc :edn
+    (one-of (keyword ext) [:clj :cljs :cljc :cljd :edn
                            ;; we needed to add this so .clj_kondo files will be
                            ;; copied from imported configs!
                            :clj_kondo])))
