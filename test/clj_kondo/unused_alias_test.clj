ec3583fd5a9ed7c1fc851a2a4c3ac8a1d0f786b9
Michiel Borkent <michielborkent@gmail.com>
2023-09-06 11:36:50 +0200

    #1332 WIP

--- test/clj_kondo/unused_alias_test.clj
+++ test/clj_kondo/unused_alias_test.clj
@@ -3,7 +3,16 @@
    [clj-kondo.test-utils :refer [lint! assert-submaps2]]
    [clojure.test :refer [deftest testing is]]))
 
+(def conf {:linters {:unused-alias {:level :warning}
+                     :unused-namespace {:level :off}}})
+
 (deftest unused-import-test
   (assert-submaps2
-   '({:file "<stdin>", :row 1, :col 34, :level :warning, :message "Unused alias: b"})
-   (lint! "(ns foo (:require [bar :as-alias b]))")))
+   '({:file "<stdin>", :row 1, :col 34, :level :warning, :message "Unused alias: b"}
+     {:file "<stdin>",
+      :row 2,
+      :col 39,
+      :level :warning,
+      :message "Unused alias: bz"})
+   (lint! "(ns foo (:require [bar :as-alias b]
+                             [baz :as bz]))" conf)))
