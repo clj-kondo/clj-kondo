34edddcf04455328610525bd2e113c33da9aedea
snasphysicist <ih8coldplay@hotmail.com>
2023-11-08 20:03:15 +0000

    feat: implement minimum version config linter

--- src/clj_kondo/impl/linters/config.clj
+++ src/clj_kondo/impl/linters/config.clj
@@ -4,7 +4,8 @@
   (:require [clj-kondo.impl.config :refer [default-config]]
             [clj-kondo.impl.findings :as findings]
             [clj-kondo.impl.linters.edn-utils :as edn-utils]
-            [clj-kondo.impl.utils :refer [sexpr node->line]]))
+            [clj-kondo.impl.utils :refer [sexpr node->line]]
+            [clj-kondo.impl.version :as version]))
 
 (set! *warn-on-reflection* true)
 
@@ -47,6 +48,58 @@
                      :clj-kondo-config
                      (str "Linter config should go under :linters")))))))
 
+(defn ^:private minimum-version-node
+  "Finds the :minimum-clj-kondo-version node in the config map"
+  [config-map]
+  (first
+   (filter
+    (fn
+      [[k _]]
+      (=
+       :min-clj-kondo-version
+       (sexpr k)))
+    config-map)))
+
+(defn ^:private compare-versions
+  "Returns a finding message if the current version
+   is below the minimum version"
+  [{minimum :minimum
+    current :current}]
+  (let [earlier-version (fn
+                          [v1 v2]
+                          (first (sort [v1 v2])))]
+    (when
+     (not=
+      minimum
+      (earlier-version
+       current
+       minimum))
+      (str
+       "Version "
+       current
+       " below configured minimum "
+       minimum))))
+
+(defn ^:private lint-minimum-version
+  "Returns a finding if this version is below the configured minimum"
+  [ctx config-map]
+  (let [[node value]  (minimum-version-node config-map)
+        minimum-version (when value (sexpr value))
+        warning (when minimum-version
+                  (compare-versions {:minimum minimum-version
+                                     :current version/version}))]
+    (when warning
+      (findings/reg-finding!
+       ctx
+       (assoc
+        (node->line
+         (:filename ctx)
+         node
+         :clj-kondo-config
+         warning)
+        :level
+        :warning)))))
+
 (defn lint-config [ctx expr]
   (try
     (let [config-edn (edn-utils/sexpr-keys expr)
@@ -56,6 +109,7 @@
           linters-map (edn-utils/node-map linters)
           _ (lint-map-vals ctx linters-map expected-linter-keys)
           _ (lint-linters ctx linters-map config-map)
+          _ (lint-minimum-version ctx config-map)
           _lint-as (:lint-as config-edn)]
       :eastwood)
     ;; Due to ubiquitous use of sexpr, we're catching coercion errors here and let them slide.
