21f932e1cb09cf5f1f7a0874a8d5c6c238df51df
snasphysicist <ih8coldplay@hotmail.com>
2023-11-09 18:54:46 +0000

    feat: tests for minimum clj kondo version linter

--- test/clj_kondo/clj_kondo_config_test.clj
+++ test/clj_kondo/clj_kondo_config_test.clj
@@ -1,7 +1,12 @@
 (ns clj-kondo.clj-kondo-config-test
   (:require
+   [clj-kondo.impl.version :as version]
    [clj-kondo.test-utils :refer [lint! assert-submaps assert-submaps2]]
-   [clojure.test :refer [deftest testing is]]))
+   [clojure.string :as str]
+   [clojure.test :refer [deftest testing is]])
+  (:import 
+   java.time.format.DateTimeFormatter
+   java.time.LocalDate))
 
 (deftest unexpected-linter-name-test
   (testing "Unexpected linter name"
@@ -29,3 +34,85 @@
      '({:file "<stdin>", :row 1, :col 1, :level :error, :message "Unresolved symbol: x"})
      (lint! "x" '{:linters {:unresolved-symbol {:exclude [(foo.bar)]
                                                     :level :error}}}))))
+
+(defn ^:private version-with-altered-date
+  "Extracts the date part of the version, applies the date-changer
+   to it (expects LocalDate argument) and returns the result
+   as a version string"
+  [date-changer]
+    (let [date-part (first (str/split
+                          version/version
+                          #"\-"))
+        date (LocalDate/parse
+              date-part
+              (DateTimeFormatter/ofPattern
+               "yyyy.MM.dd"))
+        plus-one-day (date-changer date)]
+    (.format
+     plus-one-day
+     (DateTimeFormatter/ofPattern
+      "yyyy.MM.dd"))))
+
+(defn ^:private one-day-in-future
+  "Returns a version one day in the future from the current version"
+  []
+  (version-with-altered-date
+   #(.plusDays % 1)))
+
+(defn ^:private one-day-in-past
+  "Returns a version one day in the past from the current version"
+  []
+  (version-with-altered-date
+   #(.plusDays % -1)))
+
+(deftest minimum-version-test
+  (testing "No finding when version equal to minimum"
+    (let [findings (lint!
+                    (str
+                     "{:min-clj-kondo-version \""
+                     version/version
+                     "\"}")
+                    '{:linters {:unresolved-symbol {:exclude [(foo.bar)]
+                                                    :level :error}}
+                      :min-clj-kondo-version version/version}
+                    "--filename"
+                    ".clj-kondo/config.edn")]
+      (is (empty? findings)))) 
+  (testing "No finding when version after minimum"
+    (let [findings (lint!
+                    (str
+                     "{:min-clj-kondo-version \""
+                     (one-day-in-past)
+                     "\"}")
+                    '{:linters {:unresolved-symbol {:exclude [(foo.bar)]
+                                                    :level :error}}
+                      :min-clj-kondo-version (one-day-in-past)}
+                    "--filename"
+                    ".clj-kondo/config.edn")]
+      (is (empty? findings))))
+  (testing "Find when version before minimum"
+    (let [findings (lint!
+                    (str
+                     "{:min-clj-kondo-version \""
+                     (one-day-in-future)
+                     "\"}")
+                    '{:linters {:unresolved-symbol {:exclude [(foo.bar)]
+                                                    :level :error}}
+                      :min-clj-kondo-version (one-day-in-future)}
+                    "--filename"
+                    ".clj-kondo/config.edn")
+          message (:message (first findings))]
+      (assert-submaps2
+       '({:file ".clj-kondo/config.edn"
+          :row 1
+          :col 2
+          :level :warning})
+       findings)
+      (is
+       (str/includes?
+        message
+        "Version"))
+      (is
+       (str/includes?
+        message
+        "below configured minimum")))))
