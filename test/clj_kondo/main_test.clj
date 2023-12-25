453cd7b63a15286d66019f53beab1e221be67297
Michiel Borkent <michielborkent@gmail.com>
2023-12-25 15:11:51 +0100

    it works

--- test/clj_kondo/main_test.clj
+++ test/clj_kondo/main_test.clj
@@ -1616,9 +1616,10 @@ foo/foo ;; this does use the private var
                      :level :error,
                      :message "a number is not a function"})
                   (lint! "(1 1)"))
-  (prn (lint! "(def foo \"foo\") foo (foo 1)"))
-  #_(assert-submaps []
-                  (lint! "(def foo \"foo\") (foo 1)"))
+  (assert-submaps
+   [{:file "<stdin>", :row 1, :col 17, :level :error
+     :message "Can't call a string as a function"}]
+   (lint! "(def foo \"foo\") foo (foo 1)" {:linters {:type-mismatch {:level :error}}}))
   (is (empty? (lint! "'(1 1)")))
   (is (empty? (lint! "(foo (1 1))" "--config"
                      "{:linters {:not-a-function {:skip-args [user/foo]}
