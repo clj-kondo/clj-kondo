bc43a5a528a7232ad8eccee141acb2631e24c218
Michiel Borkent <michielborkent@gmail.com>
2024-09-26 11:47:13 +0200

    wip

--- test/clj_kondo/redundant_ignore_test.clj
+++ test/clj_kondo/redundant_ignore_test.clj
@@ -1,17 +1,16 @@
 (ns clj-kondo.redundant-ignore-test
-  (:require [clj-kondo.test-utils :refer [lint! assert-submaps assert-submaps2]]
-            [clojure.java.io :as io]
+  (:require [clj-kondo.test-utils :refer [lint! assert-submaps2]]
             [clojure.test :refer [deftest is testing]]))
 
 (def config {:linters {:redundant-ignore {:level :warning}}})
 
 (deftest redundant-ignore-test
-  (assert-submaps
+  (assert-submaps2
    '({:file "<stdin>", :row 1, :col 3, :level :warning, :message "Redundant ignore"})
    (lint! "#_:clj-kondo/ignore (+ 1 2 3)" config)))
 
 (deftest redundant-ignore-unused-private-var-test
-  (assert-submaps
+  (assert-submaps2
    []
    (lint! "#_{:clj-kondo/ignore [:unused-private-var]}
 (defn- -debug [& strs]
