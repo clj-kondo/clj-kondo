6959342c6ee5a5a65251d5aaa95bc306c81d3e02
Michiel Borkent <michielborkent@gmail.com>
2024-11-14 14:22:02 +0100

    Add negative tests

--- test/clj_kondo/redundant_nested_call_test.clj
+++ test/clj_kondo/redundant_nested_call_test.clj
@@ -1,17 +1,31 @@
 (ns clj-kondo.redundant-nested-call-test
-  (:require [clj-kondo.test-utils :refer [lint! assert-submaps]]
+  (:require [clj-kondo.test-utils :refer [lint! assert-submaps2]]
             [clojure.test :refer [deftest is testing]]))
 
 (deftest redundant-nesting-test
-  (assert-submaps
+  (assert-submaps2
    '({:file "<stdin>", :row 1, :col 10, :level :warning, :message "Redundant nested call: min"})
    (lint! "(min 5 2 (min 3 7))"))
-  (assert-submaps
+  (assert-submaps2
    '({:file "<stdin>", :row 1, :col 8, :level :warning, :message "Redundant nested call: *"})
    (lint! "(* 3 4 (* 5 6))"))
-  (assert-submaps
+  (assert-submaps2
    '({:file "<stdin>", :row 1, :col 18, :level :warning, :message "Redundant nested call: str"})
    (lint! "(str \"foo\" \"bar\" (str \"baz\" \"qux\"))"))
-  (assert-submaps
+  (assert-submaps2
    '({:file "<stdin>", :row 1, :col 21, :level :warning, :message "Redundant nested call: concat"})
-   (lint! "(concat [1 2] [3 4] (concat [5 6] [7 8]))")))
+   (lint! "(concat [1 2] [3 4] (concat [5 6] [7 8]))"))
+  (assert-submaps2
+   '({:file "<stdin>", :row 1, :col 21, :level :warning, :message "Redundant nested call: concat"})
+   (lint! "(concat [1 2] [3 4] (concat [5 6] [7 8]))"))
+  (is
+   (empty?
+    (lint! "#_:clj-kondo/ignore (concat [1 2] [3 4] (concat [5 6] [7 8]))")))
+  (is
+   (empty?
+    (lint! "(ns foo) (concat [1 2] [3 4] (concat [5 6] [7 8]))"
+           '{:config-in-ns {foo {:linters {:redundant-nested-call {:level :off}}}}})))
+  (is
+   (empty?
+    (lint! "(ns foo) (concat [1 2] [3 4] (concat [5 6] [7 8]))"
+           '{:config-in-call {clojure.core/concat {:linters {:redundant-nested-call {:level :off}}}}}))))
