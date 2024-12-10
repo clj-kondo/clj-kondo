2bedcd58d0456b7cbf0c6f3381f03ca34e6a0427
Tom Dalziel <33435574+tomdl89@users.noreply.github.com>
2024-12-10 15:44:38 +0000

    Add printf to vars linted by analyze-format (#2453)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,7 @@ For a list of breaking changes, check [here](#breaking-changes).
 
 ## Unreleased
 
+- Add `printf` to vars linted by `analyze-format`. ([@tomdl89](https://github.com/tomdl89))
 - [#2272](https://github.com/clj-kondo/clj-kondo/issues/2272): Report var usage in `if-let` etc condition as always truthy
 - [#2272](https://github.com/clj-kondo/clj-kondo/issues/2272): Report var usage in `if-not` condition as always truthy
 - [#2433](https://github.com/clj-kondo/clj-kondo/issues/2433): false positive redundant ignore with hook
--- test/clj_kondo/format_test.clj
+++ test/clj_kondo/format_test.clj
@@ -17,6 +17,9 @@
   (assert-submaps
    '({:file "<stdin>", :row 1, :col 68, :level :error, :message "Format string expects 1 arguments instead of 2."})
    (lint! "(require '[clojure.tools.logging :as l]) (l/errorf (ex-info \"\" {}) \"%s\" 1 2)"))
+  (assert-submaps
+   '({:file "<stdin>", :row 1, :col 9, :level :error, :message "Format string expects 2 arguments instead of 1."})
+   (lint! "(printf \"%s %s\" 1)"))
   (is (empty? (lint! "(format \"%3$s\" 1 2 3)")))
   (is (empty? (lint! "(format \"%3$s %s %s %s\" 1 2 3)")))
   (is (empty? (lint! "(format \"%3$s %s %s %s %s\" 1 2 3 4)")))
