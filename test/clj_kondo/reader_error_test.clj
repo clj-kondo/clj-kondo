1496310164e66f290ca4686558ab9f509b75987a
Michiel Borkent <michielborkent@gmail.com>
2025-03-10 17:33:14 +0100

    wip

---
+++ test/clj_kondo/reader_error_test.clj
@@ -0,0 +1,19 @@
+(ns clj-kondo.reader-error-test
+  (:require [clj-kondo.test-utils :as tu :refer [lint!]]
+            [clojure.test :as t :refer [deftest is]]))
+
+(deftest reader-escape-char-error-test
+  (is (= [{:file "<stdin>",
+           :row 2,
+           :col 3,
+           :level :error,
+           :message "Unsupported escape character: \\a"}
+          {:file "<stdin>",
+           :row 3,
+           :col 2,
+           :level :error,
+           :message "Unsupported escape character: \\v"}]
+         (lint! "12356
+  \"\\a\" 
+ {\"\\v\" \"\\V\"}"
+                {:linters {:syntax {:level :error}}}))))
