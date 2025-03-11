43fbe86b2222741de2b8adfb869172c90d3fd713
Michiel Borkent <michielborkent@gmail.com>
2025-03-11 16:58:45 +0100

    lint

--- test/clj_kondo/unsupported_escape_character_test.clj
+++ test/clj_kondo/unsupported_escape_character_test.clj
@@ -1,7 +1,8 @@
 (ns clj-kondo.unsupported-escape-character-test
-  (:require [clj-kondo.test-utils :as tu :refer [lint! assert-submaps2]]
-            [clojure.test :as t :refer [deftest is]]
-            [clojure.java.io :as io]))
+  (:require
+   [clj-kondo.test-utils :as tu :refer [assert-submaps2 lint!]]
+   [clojure.java.io :as io]
+   [clojure.test :as t :refer [deftest is]]))
 
 (deftest reader-escape-char-error-test
   (assert-submaps2 [{:file "corpus/unsupported_escape_chars.clj", :row 2, :col 1, :level :error, :message "Unsupported escape character: \\a."}
