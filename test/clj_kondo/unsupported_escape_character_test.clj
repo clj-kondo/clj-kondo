f9b82a927c7f17435202c961f724f336c5c3e972
Michiel Borkent <michielborkent@gmail.com>
2025-03-11 16:55:18 +0100

    add tests

--- test/clj_kondo/unsupported_escape_character_test.clj
+++ test/clj_kondo/unsupported_escape_character_test.clj
@@ -1,24 +1,11 @@
 (ns clj-kondo.unsupported-escape-character-test
   (:require [clj-kondo.test-utils :as tu :refer [lint! assert-submaps2]]
-            [clojure.test :as t :refer [deftest is]]))
+            [clojure.test :as t :refer [deftest is]]
+            [clojure.java.io :as io]))
 
 (deftest reader-escape-char-error-test
-  (assert-submaps2 [{:file "<stdin>",
-                    :row 2,
-                    :col 3,
-                    :level :error,
-                    :message "Unsupported escape character: \\a."}
-                   {:file "<stdin>",
-                    :row 3,
-                    :col 3,
-                    :level :error,
-                    :message "Unsupported escape character: \\v."}
-                    {:file "<stdin>",
-                     :row 3,
-                     :col 8,
-                     :level :error,
-                     :message "Unsupported escape character: \\V."}]
-                  (lint! "12356
-  \"\\a\" 
- {\"\\v\" \"\\V\"}"
-                         {:linters {:reader-error {:level :error}}})))
+  (assert-submaps2 [{:file "corpus/unsupported_escape_chars.clj", :row 2, :col 1, :level :error, :message "Unsupported escape character: \\a."}
+                    {:file "corpus/unsupported_escape_chars.clj", :row 3, :col 2, :level :error, :message "Unsupported escape character: \\v."}
+                    {:file "corpus/unsupported_escape_chars.clj", :row 3, :col 7, :level :error, :message "Unsupported escape character: \\V."}]
+                   (lint! (io/file "corpus/unsupported_escape_chars.clj")
+                          {:linters {:reader-error {:level :error}}})))
