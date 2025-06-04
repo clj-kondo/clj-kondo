c7168ca5afda7920861048a8771f145a4e786a72
Michiel Borkent <michielborkent@gmail.com>
2025-06-04 20:57:04 +0200

    assert-submaps reversion

--- test/clj_kondo/hooks_test.clj
+++ test/clj_kondo/hooks_test.clj
@@ -10,7 +10,7 @@
 (set! *warn-on-reflection* true)
 
 (deftest macroexpand-test
-  (assert-submaps2
+  (assert-submaps
    '({:file "corpus/macroexpand.clj", :row 16, :col 7, :level :error, :message "Expected: number, received: keyword."}
      {:file "corpus/macroexpand.clj", :row 17, :col 7, :level :error, :message "Expected: number, received: string."}
      {:file "corpus/macroexpand.clj", :row 20, :col 1, :level :error, :message #"No sym and val provided"}
