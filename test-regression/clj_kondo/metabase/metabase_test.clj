d2f7a79cd0417dfff4b71d0036ecb877222fcd4d
Michiel Borkent <michielborkent@gmail.com>
2025-07-30 14:52:44 +0200

    wip

--- test-regression/clj_kondo/metabase/metabase_test.clj
+++ test-regression/clj_kondo/metabase/metabase_test.clj
@@ -4,6 +4,7 @@
    [babashka.process :as p]
    [clj-kondo.core :as clj-kondo]
    [clj-kondo.test-utils :refer [assert-submaps2]]
+   [clojure.string :as str]
    [clojure.test :as t :refer [deftest is testing]]))
 
 (deftest metabase-test
@@ -14,7 +15,12 @@
       (p/shell {:dir test-regression-checkouts} "git clone --no-checkout --depth 1 https://github.com/metabase/metabase"))
     (p/shell {:dir dir} "git fetch --depth 1 origin" "aa0cdb546d7c9e4ef5c52ad23c656272b7599e23")
     (p/shell {:dir dir} "git fetch  --depth 1 origin" "aa0cdb546d7c9e4ef5c52ad23c656272b7599e23")
-    (p/shell {:dir dir} "git checkout aa0cdb546d7c9e4ef5c52ad23c656272b7599e23 src test .clj-kondo")
+    (p/shell {:dir dir} "git checkout aa0cdb546d7c9e4ef5c52ad23c656272b7599e23 src test .clj-kondo deps.edn clj-kondo")
+    (let [cp (-> (p/shell {:dir dir :out :string} "clj -Spath") :out str/trim)]
+      (clj-kondo/run! {:config-dir config-dir ;; important to pass this to set the right dir for copy-configs!
+                       :copy-configs true
+                       :skip-lint true
+                       :lint [cp]}))
     (let [paths (mapv #(str (fs/file dir %)) ["src" #_"test"])
             config-dir (fs/file dir ".clj-kondo")
             lint-result (clj-kondo/run! {:config-dir config-dir
@@ -22,4 +28,4 @@
                                          :repro true})]
         (assert-submaps2
          []
-         (:findings lint-result)))))
+         (prn (:findings lint-result))))))
