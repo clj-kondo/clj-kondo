d8645048b4918c275843c4fc2fd72d9c9b1bb24d
Michiel Borkent <michielborkent@gmail.com>
2025-07-31 15:33:05 +0200

    modify clerk tests according to metabase test

--- test-regression/clj_kondo/nextjournal/clerk_test.clj
+++ test-regression/clj_kondo/nextjournal/clerk_test.clj
@@ -1,20 +1,25 @@
 (ns clj-kondo.nextjournal.clerk-test
   (:require
    [babashka.fs :as fs]
+   [babashka.process :as p]
    [clj-kondo.core :as clj-kondo]
-   [clj-kondo.test-utils :refer [assert-submaps2]]
    [clojure.test :as t :refer [deftest is testing]]))
 
 (deftest clerk-test
-  (let [libname 'io.github.nextjournal/clerk
-        git-sha "b0fd4458a1af7f3ecd95e0cde41761ac297e24f5"
-        git-dir (format ".gitlibs/libs/%s/%s" libname git-sha)
-        git-dir (fs/file (fs/home) git-dir)
-        paths (mapv #(str (fs/file git-dir %)) ["src" "test" "bb"])
-        config-dir (fs/file git-dir ".clj-kondo")
-        lint-result (clj-kondo/run! {:config-dir config-dir
-                                     :lint paths
-                                     :repro true})]
-    (assert-submaps2
-     []
-     (:findings lint-result))))
+  (let [test-regression-checkouts (fs/file "test-regression" "checkouts")
+        _ (fs/create-dirs test-regression-checkouts)
+        dir (fs/file test-regression-checkouts "clerk")
+        config-dir (fs/file dir ".clj-kondo")
+        sha "b0fd4458a1af7f3ecd95e0cde41761ac297e24f5"]
+    (when-not (fs/exists? dir)
+      (p/shell {:dir test-regression-checkouts} "git clone --no-checkout --depth 1 https://github.com/nextjournal/clerk"))
+
+    (p/shell {:dir dir} "git fetch --depth 1 origin" sha)
+    (p/shell {:dir dir} "git fetch  --depth 1 origin" sha)
+    (p/shell {:dir dir} "git checkout" sha "src" "resources" "test" "bb" ".clj-kondo" "deps.edn")
+    (let [paths (mapv #(str (fs/file dir %)) ["src" "test" "bb"])
+          lint-result (clj-kondo/run! {:config-dir config-dir
+                                       :lint paths
+                                       :repro true})]
+      (prn (:summary lint-result))
+      (is (empty? (:findings lint-result))))))
