acc52b94705d57aaee181d3289441a0cb1d67cab
Michiel Borkent <michielborkent@gmail.com>
2025-07-31 21:21:36 +0200

    ductile pat set

--- test-regression/clj_kondo/nextjournal/ductile_test.clj
+++ test-regression/clj_kondo/nextjournal/ductile_test.clj
@@ -3,23 +3,29 @@
    [babashka.fs :as fs]
    [babashka.process :as p]
    [clj-kondo.core :as clj-kondo]
+   [clojure.string :as str]
    [clojure.test :as t :refer [deftest is testing]]))
 
-(deftest clerk-test
-  (let [test-regression-checkouts (fs/file "test-regression" "checkouts")
-        _ (fs/create-dirs test-regression-checkouts)
-        dir (fs/file test-regression-checkouts "ductile")
-        config-dir (fs/file dir ".clj-kondo")
-        sha "1448fc66dad4550f716f35c6718ef03b0af94d9a"]
-    (when-not (fs/exists? dir)
-      (p/shell {:dir test-regression-checkouts} "git clone --no-checkout --depth 1 ssh://git@github.com/nextjournal/ductile"))
+(deftest ductile-test
+  (if (System/getenv "GITHUB_DUCTILE_PAT")
+    (let [test-regression-checkouts (fs/file "test-regression" "checkouts")
+          _ (fs/create-dirs test-regression-checkouts)
+          dir (fs/file test-regression-checkouts "ductile")
+          config-dir (fs/file dir ".clj-kondo")
+          sha "1448fc66dad4550f716f35c6718ef03b0af94d9a"]
+      (when-not (fs/exists? dir)
+        (p/shell {:dir test-regression-checkouts}
+                 (str/replace "git clone --no-checkout --depth 1 https://x-access-token:$GITHUB_DUCTILE_PAT@github.com/nextjournal/ductile"
+                              "$GITHUB_DUCTILE_PAT"
+                              (System/getenv "GITHUB_DUCTILE_PAT"))))
 
-    (p/shell {:dir dir} "git fetch --depth 1 origin" sha)
-    (p/shell {:dir dir} "git fetch  --depth 1 origin" sha)
-    (p/shell {:dir dir} "git checkout" sha "src" "resources" "dev" "test" "bb" ".clj-kondo" "deps.edn")
-    (let [paths (mapv #(str (fs/file dir %)) ["src" "test" "bb" "dev"])
-          lint-result (clj-kondo/run! {:config-dir config-dir
-                                       :lint paths
-                                       :repro true})]
-      (prn (:summary lint-result))
-      (is (empty? (:findings lint-result))))))
+      (p/shell {:dir dir} "git fetch --depth 1 origin" sha)
+      (p/shell {:dir dir} "git fetch  --depth 1 origin" sha)
+      (p/shell {:dir dir} "git checkout" sha "src" "resources" "dev" "test" "bb" ".clj-kondo" "deps.edn")
+      (let [paths (mapv #(str (fs/file dir %)) ["src" "test" "bb" "dev"])
+            lint-result (clj-kondo/run! {:config-dir config-dir
+                                         :lint paths
+                                         :repro true})]
+        (prn (:summary lint-result))
+        (is (empty? (:findings lint-result)))))
+    (println "GITHUB_DUCTILE_PAT not set, skipping ductile test")))
