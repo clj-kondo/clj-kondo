c5215b3a3fd3386c5e252545ec8eb68bc61cbab3
Michiel Borkent <michielborkent@gmail.com>
2025-08-05 13:27:49 +0200

    difftastic

--- test-regression/clj_kondo/clj_kondo/diff_test.clj
+++ test-regression/clj_kondo/clj_kondo/diff_test.clj
@@ -7,30 +7,34 @@
    [clojure.edn :as edn]
    [clojure.pprint :as pp]
    [clojure.string :as str]
-   [clojure.test :as t :refer [deftest is testing]]))
+   [clojure.test :as t :refer [deftest is testing]]
+   [matcher-combinators.config :as mconfig]))
 
 (deftest clj-kondo-diff-test
-  (let [cp (-> (p/shell {:out :string} "clojure -Spath -A:cljs:clojure-1.12.1") :out str/trim)
-        config-dir (fs/file "test-regression" "clj_kondo" "clj_kondo" ".clj-kondo")
-        _ (fs/delete-tree (fs/file config-dir ".cache"))
-        lint-result (clj-kondo/run! {:config-dir config-dir
-                                     :cache false
-                                     :parallel true
-                                     :lint [cp]
-                                     :repro true
-                                     :config {:linters
-                                              {:redundant-fn-wrapper {:level :warning}
-                                               :condition-always-true {:level :warning}
-                                               :not-a-function
-                                               '{:skip-args [clojure.pprint/defdirectives
-                                                             cljs.pprint/defdirectives
-                                                             clojure.data.json/codepoint-case]}
-                                               :def-fn {:level :warning}
-                                               :redundant-str-call {:level :warning}}
-                                              :output {:langs false}}})
-        findings (:findings lint-result)
-        findings (remove #(str/includes? (:filename %) "src/scratch") findings)
-        expected-findings-file (fs/file "test-regression" "clj_kondo" "clj_kondo" "findings.edn")
-        _ (when false (spit expected-findings-file (with-out-str (pp/pprint findings))))
-        expected (edn/read-string (slurp expected-findings-file))]
-    (assert-submaps2 expected findings)))
+  (binding [mconfig/*use-abbreviation* true]
+    (let [cp (-> (p/shell {:out :string} "clojure -Spath -A:cljs:clojure-1.12.1") :out str/trim)
+          config-dir (fs/file "test-regression" "clj_kondo" "clj_kondo" ".clj-kondo")
+          _ (fs/delete-tree (fs/file config-dir ".cache"))
+          lint-result (clj-kondo/run! {:config-dir config-dir
+                                       :cache false
+                                       :parallel true
+                                       :lint [cp]
+                                       :repro true
+                                       :config {:linters
+                                                {:redundant-fn-wrapper {:level :warning}
+                                                 :condition-always-true {:level :warning}
+                                                 :not-a-function
+                                                 '{:skip-args [clojure.pprint/defdirectives
+                                                               cljs.pprint/defdirectives
+                                                               clojure.data.json/codepoint-case]}
+                                                 :def-fn {:level :warning}
+                                                 :redundant-str-call {:level :warning}}
+                                                :output {:langs false}}})
+          findings (:findings lint-result)
+          actual (remove #(str/includes? (:filename %) "src/scratch") findings)
+          expected-findings-file (fs/file "test-regression" "clj_kondo" "clj_kondo" "findings.edn")
+          _ (when false (spit expected-findings-file (with-out-str (pp/pprint actual))))
+          expected (edn/read-string (slurp expected-findings-file))]
+      (when-not (assert-submaps2 expected actual)
+        (spit "/tmp/actual.edn" (with-out-str (pp/pprint actual)))
+        (p/shell "difft" (str expected-findings-file) "/tmp/actual.edn")))))
