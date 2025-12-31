991f7e2ac79d70cdf97a9f48f319d3df448570d4
Jon <jonurnieta@gmail.com>
2025-12-31 15:02:39 +0100

    fix test file name and extract config to a def

--- test/clj_kondo/destructured_or_always_evaluates_test.clj
+++ test/clj_kondo/destructured_or_always_evaluates_test.clj
@@ -3,6 +3,8 @@
    [clj-kondo.test-utils :refer [lint! assert-submaps2] :rename {assert-submaps2 assert-submaps}]
    [clojure.test :refer [deftest is testing]]))
 
+(def config  {:linters {:destructured-or-always-evaluates {:level :warning}}})
+
 (deftest destructured-or-always-evaluates-test
   (testing "call in :or mapping in map destructuring"
     (assert-submaps
@@ -12,7 +14,7 @@
         :level :warning
         :message "Default :or value is always evaluated"})
      (lint! "(let [{:keys [x] :or {x (f1)}} {:x 1}] x)"
-            {:linters {:destructured-or-always-evaluates {:level :warning}}})))
+            config)))
   (testing "multiple calls in :or mappings"
     (assert-submaps
      '({:row 1
@@ -22,27 +24,27 @@
         :col 34
         :message "Default :or value is always evaluated"})
      (lint! "(let [{:keys [x y] :or {x (f1) y (f2)}} {}] [x y])"
-            {:linters {:destructured-or-always-evaluates {:level :warning}}})))
+            config)))
   (testing "nested map destructuring"
     (assert-submaps
      '({:row 1
         :col 26
         :message "Default :or value is always evaluated"})
      (lint! "(let [{{:keys [b] :or {b (f1)}} :a} {}] b)"
-            {:linters {:destructured-or-always-evaluates {:level :warning}}})))
+            config)))
   (testing "vector destructuring (should not trigger)"
     (is (empty? (lint! "(let [[x y] [1 2]] x)"
-                       {:linters {:destructured-or-always-evaluates {:level :warning}}}))))
+                       config))))
   (testing "proper :or mapping (should not trigger)"
     (is (empty? (lint! "(let [{:keys [x] :or {x 1}} {:x 1}] x)"
-                       {:linters {:destructured-or-always-evaluates {:level :warning}}}))))
-  (testing "empty list in :or (should not trigger)"
-    (is (empty? (lint! "(let [{:keys [x] :or {x ()}} {:x 1}] x)"
-                       {:linters {:destructured-or-always-evaluates {:level :warning}}}))))
-  (testing "collection literal with call in :or"
-    (assert-submaps
-     '({:row 1
-        :col 29
-        :message "Default :or value is always evaluated"})
-     (lint! "(let [{:keys [x] :or {x {:y (f1)}}} {}] x)"
-            {:linters {:destructured-or-always-evaluates {:level :warning}}}))))
+                       config)))
+    (testing "empty list in :or (should not trigger)"
+      (is (empty? (lint! "(let [{:keys [x] :or {x ()}} {:x 1}] x)"
+                         config))))
+    (testing "collection literal with call in :or"
+      (assert-submaps
+       '({:row 1
+          :col 29
+          :message "Default :or value is always evaluated"})
+       (lint! "(let [{:keys [x] :or {x {:y (f1)}}} {}] x)"
+              config)))))
