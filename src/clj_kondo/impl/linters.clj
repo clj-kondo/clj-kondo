418acbb10e417dfbad1f2be32a55d16db7487feb
Hugo Duncan <hugo@hugoduncan.org>
2026-01-02 11:32:03 -0500

    Remove debug prn from linters.clj

--- src/clj_kondo/impl/linters.clj
+++ src/clj_kondo/impl/linters.clj
@@ -184,7 +184,6 @@
   (when-let [arg-types (:arg-types call)]
     (let [arg-types @arg-types
           tags (map #(tu/resolve-arg-type idacs %) arg-types)]
-      ;; (prn (:name called-fn) :tags tags)
       (types/lint-arg-types ctx called-fn arg-types tags call)
       (when (and
              (= 'str (:name called-fn))
