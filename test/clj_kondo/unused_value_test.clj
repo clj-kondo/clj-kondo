3368695a914ba0856bdcbc3a0ac0294a03c0f87b
Jon <jonurnieta@gmail.com>
2026-01-06 07:50:11 +0100

    Fix unused value linter for defmethod bodies
    Update the unused value linter to correctly allow unused values in
    defmethod bodies. This change addresses issue #2711 and ensures that
    the linter behaves as expected in these contexts.
    
    Fixes #2711

--- test/clj_kondo/unused_value_test.clj
+++ test/clj_kondo/unused_value_test.clj
@@ -182,3 +182,19 @@ bar)"
   (is (empty?
        (lint! "(fn [] '({:a 1} {:b 2} {:c 3}))"
               {:linters {:unused-value {:level :warning}}}))))
+
+(deftest defmethod-unused-value-test
+  (testing "issue #2711 - unused value should be detected in defmethod"
+    (assert-submaps
+     '({:file "<stdin>"
+        :row 1
+        :col 73
+        :level :warning
+        :message "Unused value: ctx"}
+       {:file "<stdin>"
+        :row 1
+        :col 77
+        :level :warning
+        :message "Unused value: 1"})
+     (lint! "(defmulti open-node identity) (defmethod open-node Document [ctx _node] ctx 1 (update ctx :loc identity))"
+            {:linters {:unused-value {:level :warning}}}))))
