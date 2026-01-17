380bb12f297501ff9d7d96358ca7646f1df840c5
Jon <jonurnieta@gmail.com>
2026-01-17 17:35:20 +0100

    Fix: Update type mismatch linter logic
    Enhance the type mismatch linter to allow 'is' assertions in
    specific namespaces without triggering warnings. This change
    ensures better compatibility with testing frameworks while
    maintaining linting accuracy.

--- src/clj_kondo/impl/analyzer.clj
+++ src/clj_kondo/impl/analyzer.clj
@@ -2511,7 +2511,12 @@
                   (update ctx :config config/merge-config! cfg)
                   ctx)
             arg-types (when (and resolved-namespace resolved-name
-                                 (not (linter-disabled? ctx :type-mismatch)))
+                                 (or (not (linter-disabled? ctx :type-mismatch))
+                                     (and (#{'clojure.test 'cljs.test}
+                                           resolved-namespace)
+                                          (= 'is resolved-name)
+                                          (not (linter-disabled?
+                                                ctx :is-message-not-string)))))
                         (atom []))
             ctx (assoc ctx :arg-types arg-types)]
         (when (:in-or-default? ctx)
@@ -2832,7 +2837,7 @@
                                                           'potemkin/import-vars
                                                           defined-by->lint-as)
                             ([clojure.core.async alt!] [clojure.core.async alt!!]
-                             [cljs.core.async alt!] [cljs.core.async alt!!])
+                                                       [cljs.core.async alt!] [cljs.core.async alt!!])
                             (core-async/analyze-alt!
                              (assoc ctx
                                     :analyze-expression** analyze-expression**
