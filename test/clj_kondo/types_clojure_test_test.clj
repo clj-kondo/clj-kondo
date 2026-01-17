f971a650fc20a13e35753d637a71b5266cea277b
Jon <jonurnieta@gmail.com>
2026-01-17 08:43:09 +0100

    Add tests for non-string messages with linter disabled
    Introduce additional test cases for the `is` macro to verify its
    behavior when non-string messages are used and the linter is
    disabled. This includes scenarios with inline metadata and
    ignore directives, ensuring comprehensive coverage of the
    linting behavior in various contexts.

--- test/clj_kondo/types_clojure_test_test.clj
+++ test/clj_kondo/types_clojure_test_test.clj
@@ -46,10 +46,25 @@
 
   (testing "is with non-string message when linter disabled"
     (is (empty? (lint! "(require '[clojure.test :refer [is]])
-                        (is (= 1 1) 42)
-                        (is (= 2 2) [\"not\" \"equal\"])
-                        (is (= 3 3) {:msg \"not equal\"})"
+                         (is (= 1 1) 42)
+                         (is (= 2 2) [\"not\" \"equal\"])
+                         (is (= 3 3) {:msg \"not equal\"})"
                        {:linters {:is-message-not-string {:level :off}}}))))
+
+  (testing "is with non-string message when linter disabled inline with #_:clj-kondo/ignore"
+    (is (empty? (lint! "(require '[clojure.test :refer [is]])
+                         (is (= 1 1) #_:clj-kondo/ignore 42)"
+                       {:linters {:is-message-not-string {:level :warning}}}))))
+
+  (testing "is with non-string message when linter disabled inline with metadata"
+    (is (empty? (lint! "(require '[clojure.test :refer [is]])
+                         (is (= 1 1) ^{:clj-kondo/ignore [:is-message-not-string]} 42)"
+                       {:linters {:is-message-not-string {:level :warning}}}))))
+
+  (testing "is with non-string message when all linters disabled inline with metadata"
+    (is (empty? (lint! "(require '[clojure.test :refer [is]])
+                         (is (= 1 1) ^{:clj-kondo/ignore true} 42)"
+                       {:linters {:is-message-not-string {:level :warning}}}))))
 
   (testing "is accepts string type resolved argument"
     (is (empty?
