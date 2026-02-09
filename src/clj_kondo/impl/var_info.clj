5ee73f68a6f61716528c0afb83d5fe1c3141b2ca
Jon <jonurnieta@gmail.com>
2026-02-09 09:21:16 +0100

    Refactor var_info.clj to use var-info-gen
    Updated the require statement to use a namespace alias for
    var-info-gen. This change simplifies the code by removing
    redundant definitions and ensures consistency in accessing
    core symbols and default values.

--- src/clj_kondo/impl/var_info.clj
+++ src/clj_kondo/impl/var_info.clj
@@ -1,22 +1,17 @@
 (ns clj-kondo.impl.var-info
   {:no-doc true}
-  (:require
-   [clj-kondo.impl.var-info-gen :refer [cljs-core-syms
-                                        clojure-core-syms
-                                        default-fq-imports
-                                        default-import->qname
-                                        unused-values]]))
+  (:require [clj-kondo.impl.var-info-gen :as var-info-gen]))
 
-(def default-fq-imports default-fq-imports)
-(def default-import->qname default-import->qname)
-(def unused-values unused-values)
+(def default-fq-imports var-info-gen/default-fq-imports)
+(def default-import->qname var-info-gen/default-import->qname)
+(def unused-values var-info-gen/unused-values)
 ;; in addition to what `special-form?` regards as special:
 (def special-forms '#{.. let fn loop})
 
 (defn core-sym? [lang sym]
   (case lang
-    :clj (contains? clojure-core-syms sym)
-    :cljs (contains? cljs-core-syms sym)))
+    :clj (contains? var-info-gen/clojure-core-syms sym)
+    :cljs (contains? var-info-gen/cljs-core-syms sym)))
 
 ;;;; Scratch
 
