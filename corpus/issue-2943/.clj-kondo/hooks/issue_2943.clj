993d26f06ed6d36f04c272a475dd0c5365e992da
Michiel Borkent <michielborkent@gmail.com>
2026-08-04 15:15:14 +0200

    Use a unique hook ns in the #2943 corpus

--- corpus/issue-2943/.clj-kondo/hooks/issue_2943.clj
+++ corpus/issue-2943/.clj-kondo/hooks/issue_2943.clj
@@ -1,4 +1,6 @@
-(ns hooks
+;; a unique ns: hook namespaces share one SCI context, so a plain `hooks` would
+;; clash with the other corpus hooks of that name
+(ns hooks.issue-2943
   (:require [clj-kondo.hooks-api :as api]))
 
 (defn dispatch [{:keys [node]}]
